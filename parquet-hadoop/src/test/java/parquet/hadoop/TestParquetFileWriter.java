251855be5bf1000d00371f4be7a04f3c83b438e4
julien <julien@twitter.com>
2013-03-11 09:49:42 -0700

    better metadata file tests

--- parquet-hadoop/src/test/java/parquet/hadoop/TestParquetFileWriter.java
+++ parquet-hadoop/src/test/java/parquet/hadoop/TestParquetFileWriter.java
@@ -26,6 +26,7 @@ import java.util.Arrays;
 import java.util.HashMap;
 
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.junit.Test;
 
@@ -44,9 +45,9 @@ public class TestParquetFileWriter {
   private static final Log LOG = Log.getLog(TestParquetFileWriter.class);
 
   @Test
-  public void test() throws Exception {
+  public void testWriteRead() throws Exception {
 
-    File testFile = new File("target/testParquetFile").getAbsoluteFile();
+    File testFile = new File("target/test/TestParquetFileWriter/testParquetFile").getAbsoluteFile();
     testFile.delete();
 
     Path path = new Path(testFile.toURI());
@@ -120,6 +121,64 @@ public class TestParquetFileWriter {
     }
     PrintFooter.main(new String[] {path.toString()});
   }
+
+  @Test
+  public void testMetaDataFile() throws Exception {
+
+    File testDir = new File("target/test/TestParquetFileWriter/testMetaDataFileDir").getAbsoluteFile();
+
+    Path testDirPath = new Path(testDir.toURI());
+    Configuration configuration = new Configuration();
+
+    final FileSystem fs = testDirPath.getFileSystem(configuration);
+    fs.delete(testDirPath, true);
+    fs.mkdirs(testDirPath);
+
+    createFile(new Path(testDirPath, "part0"));
+    createFile(new Path(testDirPath, "part1"));
+    createFile(new Path(testDirPath, "part2"));
+
+
+    MessageType schema = MessageTypeParser.parseMessageType("message m { required group a {required binary b;} required group c { required int64 d; }}");
+    String[] path1 = {"a", "b"};
+    ColumnDescriptor c1 = schema.getColumnDescription(path1);
+    String[] path2 = {"c", "d"};
+    ColumnDescriptor c2 = schema.getColumnDescription(path2);
+
+    byte[] bytes1 = { 0, 1, 2, 3};
+    byte[] bytes2 = { 1, 2, 3, 4};
+    byte[] bytes3 = { 2, 3, 4, 5};
+    byte[] bytes4 = { 3, 4, 5, 6};
+    CompressionCodecName codec = CompressionCodecName.UNCOMPRESSED;
+    ParquetFileWriter w = new ParquetFileWriter(configuration, schema, path);
+    w.start();
+    w.startBlock(3);
+    w.startColumn(c1, 5, codec);
+    w.writeDataPage(2, 4, BytesInput.from(bytes1), PLAIN);
+    w.writeDataPage(3, 4, BytesInput.from(bytes1), PLAIN);
+    w.endColumn();
+    w.startColumn(c2, 6, codec);
+    w.writeDataPage(2, 4, BytesInput.from(bytes2), PLAIN);
+    w.writeDataPage(3, 4, BytesInput.from(bytes2), PLAIN);
+    w.writeDataPage(1, 4, BytesInput.from(bytes2), PLAIN);
+    w.endColumn();
+    w.endBlock();
+    w.startBlock(4);
+    w.startColumn(c1, 7, codec);
+    w.writeDataPage(7, 4, BytesInput.from(bytes3), PLAIN);
+    w.endColumn();
+    w.startColumn(c2, 8, codec);
+    w.writeDataPage(8, 4, BytesInput.from(bytes4), PLAIN);
+    w.endColumn();
+    w.endBlock();
+    w.end(new HashMap<String, String>());
+
+    ParquetMetadata readFooter = ParquetFileReader.readFooter(configuration, path);
+    assertEquals("footer: "+readFooter, 2, readFooter.getBlocks().size());
+
+
+  }
+
 
   private void validateContains(MessageType schema, PageReadStore pages, String[] path, int values, BytesInput bytes) throws IOException {
     PageReader pageReader = pages.getPageReader(schema.getColumnDescription(path));
