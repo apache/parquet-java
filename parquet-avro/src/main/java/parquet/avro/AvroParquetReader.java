e365840ea62f29a6cca472772bf96bee8b1a4e3c
Tom White <tom@cloudera.com>
2013-04-22 11:54:37 +0100

    Create generic Parquet reader and writer for object records.

--- parquet-avro/src/main/java/parquet/avro/AvroParquetReader.java
+++ parquet-avro/src/main/java/parquet/avro/AvroParquetReader.java
@@ -1,65 +1,31 @@
+/**
+ * Copyright 2012 Twitter, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package parquet.avro;
 
-import java.io.Closeable;
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
-import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.fs.FileStatus;
-import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.mapreduce.TaskAttemptContext;
-import org.apache.hadoop.mapreduce.TaskAttemptID;
-import parquet.hadoop.Footer;
-import parquet.hadoop.ParquetFileReader;
-import parquet.hadoop.ParquetInputSplit;
-import parquet.hadoop.ParquetRecordReader;
+import parquet.hadoop.ParquetReader;
 import parquet.hadoop.api.ReadSupport;
-import parquet.hadoop.metadata.BlockMetaData;
-import parquet.hadoop.metadata.FileMetaData;
 
 /**
  * Read Avro records from a Parquet file.
  */
-public class AvroParquetReader<T> implements Closeable {
-
-  private ParquetRecordReader<T> reader;
+public class AvroParquetReader<T> extends ParquetReader<T> {
 
   public AvroParquetReader(Path file) throws IOException {
-    Configuration conf = new Configuration();
-    reader = new ParquetRecordReader<T>((ReadSupport<T>) new
-        AvroReadSupport()); // TODO: remove cast
-
-    FileSystem fs = FileSystem.get(conf);
-    List<FileStatus> statuses = Arrays.asList(fs.listStatus(file));
-    List<Footer> footers = ParquetFileReader.readAllFootersInParallelUsingSummaryFiles(conf, statuses);
-    Footer footer = footers.get(0); // TODO: check only one
-
-    List<BlockMetaData> blocks = footer.getParquetMetadata().getBlocks();
-    FileMetaData fileMetaData = footer.getParquetMetadata().getFileMetaData();
-    String schema = fileMetaData.getSchema().toString();
-    Map<String, String> extraMetadata = fileMetaData.getKeyValueMetaData();
-    ParquetInputSplit inputSplit = new ParquetInputSplit(file, 0, 0, null, blocks,
-        schema, schema, extraMetadata);
-    TaskAttemptContext taskAttemptContext = new TaskAttemptContext(conf, new TaskAttemptID("test", 1, true, 1, 1));
-    try {
-      reader.initialize(inputSplit, taskAttemptContext);
-    } catch (InterruptedException e) {
-      throw new IOException(e);
-    }
-  }
-
-  public T read() throws IOException {
-    try {
-      return reader.nextKeyValue() ? reader.getCurrentValue() : null;
-    } catch (InterruptedException e) {
-      throw new IOException(e);
-    }
-  }
-
-  @Override
-  public void close() throws IOException {
-    reader.close();
+    super(file, (ReadSupport<T>) new AvroReadSupport());
   }
 }
--- parquet-avro/src/main/java/parquet/avro/AvroParquetWriter.java
+++ parquet-avro/src/main/java/parquet/avro/AvroParquetWriter.java
@@ -1,57 +1,34 @@
+/**
+ * Copyright 2012 Twitter, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package parquet.avro;
 
-import java.io.Closeable;
 import java.io.IOException;
 import org.apache.avro.Schema;
-import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
-import parquet.hadoop.CodecFactory;
-import parquet.hadoop.ParquetFileWriter;
-import parquet.hadoop.ParquetRecordWriter;
+import parquet.hadoop.ParquetWriter;
 import parquet.hadoop.api.WriteSupport;
-import parquet.hadoop.metadata.CompressionCodecName;
-import parquet.schema.MessageType;
 
 /**
  * Write Avro records to a Parquet file.
  */
-public class AvroParquetWriter<T> implements Closeable {
-
-  private final ParquetRecordWriter<T> writer;
+public class AvroParquetWriter<T> extends ParquetWriter<T> {
 
   public AvroParquetWriter(Path file, Schema avroSchema) throws IOException {
-    Configuration conf = new Configuration();
-    MessageType schema = new AvroSchemaConverter().convert(avroSchema);
-
-    ParquetFileWriter fileWriter = new ParquetFileWriter(conf, schema, file);
-
-    fileWriter.start();
-
-    WriteSupport<T> writeSupport = (WriteSupport<T>) new AvroWriteSupport(schema,
-        avroSchema); // TODO remove cast
-    WriteSupport.WriteContext writeContext = writeSupport.init(conf);
-
-    CodecFactory codecFactory = new CodecFactory(conf);
-    CodecFactory.BytesCompressor compressor = codecFactory.getCompressor(CompressionCodecName.UNCOMPRESSED, 0);
-    this.writer = new ParquetRecordWriter<T>
-        (fileWriter, writeSupport, schema, writeContext.getExtraMetaData(), 50*1024*1024,
-            1*1024*1024, compressor);
+    super(file, (WriteSupport<T>)
+        new AvroWriteSupport(new AvroSchemaConverter().convert(avroSchema), avroSchema));
   }
 
-  public void write(T object) throws IOException {
-    try {
-      writer.write(null, object);
-    } catch (InterruptedException e) {
-      throw new IOException(e);
-    }
-  }
-
-  @Override
-  public void close() throws IOException {
-    try {
-      writer.close(null);
-    } catch (InterruptedException e) {
-      throw new IOException(e);
-    }
-  }
 }
