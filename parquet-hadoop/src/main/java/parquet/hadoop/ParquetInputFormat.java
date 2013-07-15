5a5bb7f26efe4a31d0de99e6b4c81199752cc118
Aniket Mokashi <amokashi@twitter.com>
2013-07-15 12:11:35 -0700

    initial commit for recursive listing

--- parquet-hadoop/src/main/java/parquet/hadoop/ParquetInputFormat.java
+++ parquet-hadoop/src/main/java/parquet/hadoop/ParquetInputFormat.java
@@ -29,6 +29,7 @@ import org.apache.hadoop.fs.BlockLocation;
 import org.apache.hadoop.fs.FileStatus;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.fs.PathFilter;
 import org.apache.hadoop.mapred.JobConf;
 import org.apache.hadoop.mapreduce.InputSplit;
 import org.apache.hadoop.mapreduce.Job;
@@ -255,6 +256,63 @@ public class ParquetInputFormat<T> extends FileInputFormat<Void, T> {
     }
     return splits;
   }
+  
+  /*
+   * This is to support multi-level/recursive directory listing until 
+   * MAPREDUCE-1577 is fixed.
+   */
+  @Override
+  protected List<FileStatus> listStatus(JobContext job) throws IOException {        
+    return getAllFileRecursively(super.listStatus(job), 
+        job.getConfiguration());             
+  }
+
+  /**
+   * Get all files recursively from the given list of files
+   * 
+   * @param files a list of FileStatus
+   * @param conf the configuration object
+   * @return the list of fileStatus that contains all the files in the given
+   *         list and, recursively, all the files inside the directories in 
+   *         the given list
+   * @throws IOException
+   */
+  private static List<FileStatus> getAllFileRecursively(
+      List<FileStatus> files, Configuration conf) throws IOException {
+    List<FileStatus> result = new ArrayList<FileStatus>();
+    int len = files.size();
+    for (int i = 0; i < len; ++i) {
+      FileStatus file = files.get(i);
+      if (file.isDir()) {
+        Path p = file.getPath();
+        FileSystem fs = p.getFileSystem(conf);
+        addInputPathRecursively(result, fs, p, hiddenFileFilter);
+      } else {
+        result.add(file);
+      }
+    }
+    LOG.info("Total input paths to process : " + result.size()); 
+    return result;
+  }
+
+  private static void addInputPathRecursively(List<FileStatus> result,
+      FileSystem fs, Path path, PathFilter inputFilter) 
+          throws IOException {
+    for (FileStatus stat: fs.listStatus(path, inputFilter)) {
+      if (stat.isDir()) {
+        addInputPathRecursively(result, fs, stat.getPath(), inputFilter);
+      } else {
+        result.add(stat);
+      }
+    }
+  }          
+
+  private static final PathFilter hiddenFileFilter = new PathFilter(){
+    public boolean accept(Path p){
+      String name = p.getName(); 
+      return !name.startsWith("_") && !name.startsWith("."); 
+    }
+  };   
 
   /**
    * @param jobContext the current job context
@@ -263,7 +321,7 @@ public class ParquetInputFormat<T> extends FileInputFormat<Void, T> {
    */
   public List<Footer> getFooters(JobContext jobContext) throws IOException {
     if (footers == null) {
-      footers = getFooters(ContextUtil.getConfiguration(jobContext), super.listStatus(jobContext));
+      footers = getFooters(ContextUtil.getConfiguration(jobContext), listStatus(jobContext));
     }
 
     return footers;
