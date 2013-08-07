a54414c908d68125994c9d3199f95000dc53db02
Tianshuo Deng <tdeng@twitter.com>
2013-08-07 15:41:48 -0700

    use Mockito to mock varibles in test, fix format and variable name

--- parquet-cascading/src/test/java/parquet/cascading/ParquetTBaseSchemeTest.java
+++ parquet-cascading/src/test/java/parquet/cascading/ParquetTBaseSchemeTest.java
@@ -8,12 +8,12 @@ import org.apache.hadoop.mapred.OutputCollector;
 import org.apache.hadoop.mapred.RecordReader;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
 import parquet.hadoop.api.ReadSupport;
 import parquet.hadoop.thrift.ThriftReadSupport;
 import parquet.io.api.RecordMaterializer;
 import parquet.schema.MessageType;
 import parquet.thrift.TBaseRecordConverter;
-
 import java.util.HashMap;
 import java.util.Map;
 
@@ -21,23 +21,23 @@ public class ParquetTBaseSchemeTest {
 
   @Test
   public void testGetRecordMaterializer() throws Exception {
-    ThriftReadSupport<String> mySupport = new ThriftReadSupport<String>();
+    ThriftReadSupport<String> readSupport = new ThriftReadSupport<String>();
     ParquetTBaseScheme tBaseScheme = new ParquetTBaseScheme();
     String descriptorKey = "thrift.descriptor";
     String mockDescriptor = "{\"id\":\"STRUCT\"}";
-    FlowProcess<JobConf> fp = null;
-    Tap<JobConf, RecordReader, OutputCollector> tap = null;
-    JobConf jobConf = new JobConf();
     Map<String, String> metaData = new HashMap<String, String>();
-    jobConf.set(ThriftReadSupport.THRIFT_READ_CLASS_KEY, AddressBook.class.getName());
     metaData.put(descriptorKey, mockDescriptor);
+    FlowProcess<JobConf> flowProcess = mock(FlowProcess.class);
+    Tap<JobConf, RecordReader, OutputCollector> tap = mock(Tap.class);
+    JobConf jobConf = new JobConf();
+    jobConf.set(ThriftReadSupport.THRIFT_READ_CLASS_KEY, AddressBook.class.getName());
     MessageType fileSchema = new MessageType("Test");
     ReadSupport.ReadContext readContext = new ReadSupport.ReadContext(fileSchema);
 
     //Mock the method call by cascading
-    tBaseScheme.sourceConfInit(fp, tap, jobConf);
+    tBaseScheme.sourceConfInit(flowProcess, tap, jobConf);
 
-    RecordMaterializer recordMaterializer = mySupport.prepareForRead(jobConf, metaData, fileSchema, readContext);
+    RecordMaterializer recordMaterializer = readSupport.prepareForRead(jobConf, metaData, fileSchema, readContext);
     assertNotNull(recordMaterializer);
     assertEquals(TBaseRecordConverter.class, recordMaterializer.getClass());
   }
