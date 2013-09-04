bb06859085679798bca20b8c54f20f2e9e006082
Tianshuo Deng <tdeng@twitter.com>
2013-09-04 15:20:52 -0700

    add license and comment

--- parquet-thrift/src/test/java/parquet/thrift/projection/PathGlobPatternTest.java
+++ parquet-thrift/src/test/java/parquet/thrift/projection/PathGlobPatternTest.java
@@ -1,3 +1,19 @@
+/**
+ * Copyright 2012 Twitter, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package parquet.thrift.projection;
 
 import org.junit.Test;
@@ -5,7 +21,9 @@ import org.junit.Test;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
-
+/**
+ * Test using glob syntax to specify which attribute to retain
+ */
 public class PathGlobPatternTest {
   @Test
   public void testRecursiveGlob() {
