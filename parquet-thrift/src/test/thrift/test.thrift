2043741be98735aeea75c2f6eb04c78220811c0a
Tianshuo Deng <tdeng@twitter.com>
2013-09-06 11:27:25 -0700

    add thrift idl for testing

--- parquet-thrift/src/test/thrift/test.thrift
+++ parquet-thrift/src/test/thrift/test.thrift
@@ -3,3 +3,21 @@ struct TestListsInMap {
   1: string name,
   2: map<list<string>,list<string>> names,
 }
+
+struct Name {
+  1: required string first_name,
+  2: string last_name
+}
+
+struct Address {
+  1: string street,
+  2: required string zip
+}
+
+struct TestPerson {
+  1: required Name name,
+  2: optional i32 age,
+  3: Address address
+
+}
+
