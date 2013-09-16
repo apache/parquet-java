2a2696dcee58fb7fd6d9072b97f0411b5ceb9470
Tianshuo Deng <tdeng@twitter.com>
2013-09-16 13:36:30 -0700

    format

--- parquet-scrooge/src/test/java/parquet/scrooge/ParquetScroogeSchemeTest.java
+++ parquet-scrooge/src/test/java/parquet/scrooge/ParquetScroogeSchemeTest.java
@@ -1,77 +1,59 @@
 package parquet.scrooge;
 
-import com.twitter.data.proto.tutorial.thrift.AddressBook;
-import com.twitter.data.proto.tutorial.thrift.Name;
-import com.twitter.data.proto.tutorial.thrift.Person;
-import com.twitter.data.proto.tutorial.thrift.PhoneNumber;
-import com.twitter.elephantbird.thrift.TStructDescriptor;
-import com.twitter.scrooge.ThriftStructCodec;
-import com.twitter.scrooge.ThriftStructField;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.BytesWritable;
 import org.apache.hadoop.mapreduce.*;
-import org.apache.hadoop.record.meta.TypeID;
 import org.apache.thrift.TBase;
-import org.apache.thrift.TEnum;
 import org.apache.thrift.protocol.TCompactProtocol;
 import org.apache.thrift.protocol.TProtocol;
 import org.apache.thrift.protocol.TProtocolFactory;
 import org.apache.thrift.transport.TIOStreamTransport;
 import org.junit.Test;
-import parquet.Log;
-import parquet.hadoop.api.ReadSupport;
 import parquet.hadoop.thrift.ParquetThriftInputFormat;
-import parquet.hadoop.thrift.TestParquetToThriftReadProjection;
 import parquet.hadoop.thrift.ThriftReadSupport;
 import parquet.hadoop.thrift.ThriftToParquetFileWriter;
 import parquet.hadoop.util.ContextUtil;
 import parquet.scrooge.test.TestPersonWithAllInformation;
-import parquet.thrift.struct.ThriftType;
-import parquet.thrift.struct.ThriftTypeID;
-import parquet.thrift.test.*;
-import scala.collection.JavaConversions;
+import parquet.thrift.test.Address;
+import parquet.thrift.test.Phone;
+import parquet.thrift.test.RequiredPrimitiveFixture;
 
 import java.io.ByteArrayOutputStream;
-import java.lang.reflect.ParameterizedType;
-import java.lang.reflect.Type;
-import java.util.*;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import static org.junit.Assert.assertEquals;
 
 /**
  * Write data in thrift, read in scrooge
+ *
+ * @author Tianshuo Deng
  */
 public class ParquetScroogeSchemeTest {
 
   @Test
-  public void testTraverse() throws Exception{
-    new ScroogeSchemaConverter().convert(TestPersonWithAllInformation.class);
-//    traverseStruct(parquet.scrooge.test.RequiredPrimitiveFixture.class.getName());
-  }
-
-  @Test
-  public void testNestedReadingInScrooge() throws Exception{
-    Map<String,parquet.thrift.test.Phone> phoneMap=new HashMap<String,Phone>();
-    phoneMap.put("key1",new parquet.thrift.test.Phone("111","222"));
-    parquet.thrift.test.TestPersonWithAllInformation toWrite= new parquet.thrift.test.TestPersonWithAllInformation(new parquet.thrift.test.Name("first"),new Address("my_street","my_zip"),"my_info",phoneMap);
+  public void testNestedReadingInScrooge() throws Exception {
+    Map<String, parquet.thrift.test.Phone> phoneMap = new HashMap<String, Phone>();
+    phoneMap.put("key1", new parquet.thrift.test.Phone("111", "222"));
+    parquet.thrift.test.TestPersonWithAllInformation toWrite = new parquet.thrift.test.TestPersonWithAllInformation(new parquet.thrift.test.Name("first"), new Address("my_street", "my_zip"), "my_info", phoneMap);
     String expected = "TestPersonWithAllInformation(Name(first,None),None,Address(my_street,my_zip),None,my_info,Map(key1 -> Phone(111,222)),None,None)";
-    verifyScroogeRead(toWrite,TestPersonWithAllInformation.class, expected);
+    verifyScroogeRead(toWrite, TestPersonWithAllInformation.class, expected);
   }
-
 
   @Test
-  public void testWritePrimitveThriftReadScrooge() throws Exception{
-    RequiredPrimitiveFixture toWrite= new RequiredPrimitiveFixture(true,(byte)2,(short)3,4,(long)5,(double)6.0,"7");
+  public void testWritePrimitveThriftReadScrooge() throws Exception {
+    RequiredPrimitiveFixture toWrite = new RequiredPrimitiveFixture(true, (byte) 2, (short) 3, 4, (long) 5, (double) 6.0, "7");
     toWrite.setInfo_string("it's info");
-    verifyScroogeRead(toWrite,parquet.scrooge.test.RequiredPrimitiveFixture.class,"RequiredPrimitiveFixture(true,2,3,4,5,6.0,7,Some(it's info))");
+    verifyScroogeRead(toWrite, parquet.scrooge.test.RequiredPrimitiveFixture.class, "RequiredPrimitiveFixture(true,2,3,4,5,6.0,7,Some(it's info))");
   }
 
-  public <T> void verifyScroogeRead(TBase recordToWrite, Class<T> readClass, String expectedStr) throws Exception{
+  public <T> void verifyScroogeRead(TBase recordToWrite, Class<T> readClass, String expectedStr) throws Exception {
     Configuration conf = new Configuration();
-    conf.set("parquet.thrift.converter.class",ScroogeRecordConverter.class.getCanonicalName());
-    conf.set(ThriftReadSupport.THRIFT_READ_CLASS_KEY,readClass.getName());
+    conf.set("parquet.thrift.converter.class", ScroogeRecordConverter.class.getCanonicalName());
+    conf.set(ThriftReadSupport.THRIFT_READ_CLASS_KEY, readClass.getName());
 
     final Path parquetFile = new Path("target/test/TestParquetToThriftReadProjection/file.parquet");
     final FileSystem fs = parquetFile.getFileSystem(conf);
@@ -82,7 +64,7 @@ public class ParquetScroogeSchemeTest {
     //create a test file
     final TProtocolFactory protocolFactory = new TCompactProtocol.Factory();
     final TaskAttemptID taskId = new TaskAttemptID("local", 0, true, 0, 0);
-    Class writeClass=recordToWrite.getClass();
+    Class writeClass = recordToWrite.getClass();
     final ThriftToParquetFileWriter w = new ThriftToParquetFileWriter(parquetFile, new TaskAttemptContext(conf, taskId), protocolFactory, writeClass);
     final ByteArrayOutputStream baos = new ByteArrayOutputStream();
     final TProtocol protocol = protocolFactory.getProtocol(new TIOStreamTransport(baos));
@@ -106,8 +88,6 @@ public class ParquetScroogeSchemeTest {
         readValue = reader.getCurrentValue();
       }
     }
-   assertEquals(expectedStr,readValue.toString());
+    assertEquals(expectedStr, readValue.toString());
   }
-
-
 }
