3105009fe85cb78922e9f70ac52297cccc9101c9
Colin Marc <colinmarc@gmail.com>
2013-09-20 21:55:44 -0500

    add read and write tests for ParquetTBaseScheme

--- parquet-cascading/src/test/java/parquet/cascading/TestParquetTBaseScheme.java
+++ parquet-cascading/src/test/java/parquet/cascading/TestParquetTBaseScheme.java
@@ -15,34 +15,65 @@
  */
 package parquet.cascading;
 
+import cascading.flow.Flow;
 import cascading.flow.FlowProcess;
+import cascading.flow.hadoop.HadoopFlowConnector;
+import cascading.operation.BaseOperation;
+import cascading.operation.Function;
+import cascading.operation.FunctionCall;
+import cascading.pipe.Each;
+import cascading.pipe.Pipe;
+import cascading.scheme.Scheme;
 import cascading.scheme.SinkCall;
+import cascading.scheme.hadoop.TextLine;
 import cascading.tap.Tap;
-import com.twitter.data.proto.tutorial.thrift.AddressBook;
+import cascading.tap.hadoop.Hfs;
+import cascading.tuple.Fields;
+import cascading.tuple.Tuple;
+import cascading.tuple.TupleEntry;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.BytesWritable;
 import org.apache.hadoop.mapred.JobConf;
 import org.apache.hadoop.mapred.OutputCollector;
 import org.apache.hadoop.mapred.RecordReader;
+import org.apache.hadoop.mapreduce.TaskAttemptContext;
+import org.apache.hadoop.mapreduce.TaskAttemptID;
+import org.apache.thrift.TBase;
+import org.apache.thrift.protocol.TCompactProtocol;
+import org.apache.thrift.protocol.TProtocol;
+import org.apache.thrift.protocol.TProtocolFactory;
+import org.apache.thrift.transport.TIOStreamTransport;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.mock;
-
 import parquet.hadoop.api.ReadSupport;
 import parquet.hadoop.thrift.ThriftReadSupport;
+import parquet.hadoop.thrift.ThriftToParquetFileWriter;
 import parquet.io.api.RecordMaterializer;
 import parquet.schema.MessageType;
 import parquet.thrift.TBaseRecordConverter;
+import parquet.thrift.test.Name;
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
 public class TestParquetTBaseScheme {
+  final String txtInputPath = "src/test/resources/names.txt";
+  final String parquetInputPath = "target/test/ParquetTBaseScheme/names-parquet-in";
+  final String parquetOutputPath = "target/test/ParquetTBaseScheme/names-parquet-out";
+  final String txtOutputPath = "target/test/ParquetTBaseScheme/names-txt-out";
 
   @Test
   public void testGetRecordMaterializer() throws Exception {
-    ThriftReadSupport<AddressBook> readSupport = new ThriftReadSupport<AddressBook>();
-    ParquetTBaseScheme tBaseScheme = new ParquetTBaseScheme(AddressBook.class);
+    ThriftReadSupport<Name> readSupport = new ThriftReadSupport<Name>();
+    ParquetTBaseScheme tBaseScheme = new ParquetTBaseScheme(Name.class);
     String descriptorKey = "thrift.descriptor";
     String mockDescriptor = "{\"id\":\"STRUCT\"}";
     Map<String, String> metaData = new HashMap<String, String>();
@@ -50,7 +81,7 @@ public class TestParquetTBaseScheme {
     FlowProcess<JobConf> flowProcess = mock(FlowProcess.class);
     Tap<JobConf, RecordReader, OutputCollector> tap = mock(Tap.class);
     JobConf jobConf = new JobConf();
-    jobConf.set(ThriftReadSupport.THRIFT_READ_CLASS_KEY, AddressBook.class.getName());
+    jobConf.set(ThriftReadSupport.THRIFT_READ_CLASS_KEY, Name.class.getName());
     MessageType fileSchema = new MessageType("Test");
     ReadSupport.ReadContext readContext = new ReadSupport.ReadContext(fileSchema);
 
@@ -60,5 +91,117 @@ public class TestParquetTBaseScheme {
     RecordMaterializer recordMaterializer = readSupport.prepareForRead(jobConf, metaData, fileSchema, readContext);
     assertNotNull(recordMaterializer);
     assertEquals(TBaseRecordConverter.class, recordMaterializer.getClass());
+  }
+
+  @Test
+  public void testWrite() throws Exception {
+    Path path = new Path(parquetOutputPath);
+    final FileSystem fs = path.getFileSystem(new Configuration());
+    if (fs.exists(path)) fs.delete(path, true);
+
+    Scheme sourceScheme = new TextLine( new Fields( "first", "last" ) );
+    Tap source = new Hfs(sourceScheme, txtInputPath);
+
+    Scheme sinkScheme = new ParquetTBaseScheme(Name.class);
+    Tap sink = new Hfs(sinkScheme, parquetOutputPath);
+
+    Pipe assembly = new Pipe( "namecp" );
+    assembly = new Each(assembly, new PackThriftFunction());
+    Flow flow  = new HadoopFlowConnector().connect("namecp", source, sink, assembly);
+
+    flow.complete();
+  }
+
+  @Test
+  public void testRead() throws Exception {
+    doRead(new ParquetTBaseScheme(Name.class));
+  }
+
+  @Test
+  public void testReadWithoutClass() throws Exception {
+    doRead(new ParquetTBaseScheme());
+  }
+
+  private void doRead(Scheme sourceScheme) throws Exception {
+    createFileForRead();
+
+    Path path = new Path(txtOutputPath);
+    final FileSystem fs = path.getFileSystem(new Configuration());
+    if (fs.exists(path)) fs.delete(path, true);
+
+    Tap source = new Hfs(sourceScheme, parquetInputPath);
+
+    Scheme sinkScheme = new TextLine(new Fields("first", "last"));
+    Tap sink = new Hfs(sinkScheme, txtOutputPath);
+
+    Pipe assembly = new Pipe( "namecp" );
+    assembly = new Each(assembly, new UnpackThriftFunction());
+    Flow flow  = new HadoopFlowConnector().connect("namecp", source, sink, assembly);
+
+    flow.complete();
+  }
+
+
+  private void createFileForRead() throws Exception {
+    final Path fileToCreate = new Path(parquetInputPath+"/names.parquet");
+
+    final Configuration conf = new Configuration();
+    final FileSystem fs = fileToCreate.getFileSystem(conf);
+    if (fs.exists(fileToCreate)) fs.delete(fileToCreate, true);
+
+    TProtocolFactory protocolFactory = new TCompactProtocol.Factory();
+    TaskAttemptID taskId = new TaskAttemptID("local", 0, true, 0, 0);
+    ThriftToParquetFileWriter w = new ThriftToParquetFileWriter(fileToCreate, new TaskAttemptContext(conf, taskId), protocolFactory, Name.class);
+
+    final ByteArrayOutputStream baos = new ByteArrayOutputStream();
+    final TProtocol protocol = protocolFactory.getProtocol(new TIOStreamTransport(baos));
+
+    Name n1 = new Name();
+    n1.setFirst_name("Alice");
+    n1.setLast_name("Practice");
+    Name n2 = new Name();
+    n2.setFirst_name("Bob");
+    n2.setLast_name("Hope");
+    Name n3 = new Name();
+    n3.setFirst_name("Charlie");
+    n3.setLast_name("Horse");
+
+    n1.write(protocol);
+    w.write(new BytesWritable(baos.toByteArray()));
+    baos.reset();
+    n2.write(protocol);
+    w.write(new BytesWritable(baos.toByteArray()));
+    baos.reset();
+    n3.write(protocol);
+    w.write(new BytesWritable(baos.toByteArray()));
+    w.close();
+  }
+
+  private static class PackThriftFunction extends BaseOperation implements Function {
+    @Override
+    public void operate(FlowProcess flowProcess, FunctionCall functionCall) {
+      TupleEntry arguments = functionCall.getArguments();
+      Tuple result = new Tuple();
+
+      Name name = new Name();
+      name.setFirst_name(arguments.getString(0));
+      name.setLast_name(arguments.getString(1));
+
+      result.add(name);
+      functionCall.getOutputCollector().add(result);
+    }
+  }
+
+  private static class UnpackThriftFunction extends BaseOperation implements Function {
+    @Override
+    public void operate(FlowProcess flowProcess, FunctionCall functionCall) {
+      TupleEntry arguments = functionCall.getArguments();
+      Tuple result = new Tuple();
+
+      Name name = (Name) arguments.get(0);
+      result.add(name.getFirst_name());
+      result.add(name.getLast_name());
+      functionCall.getOutputCollector().add(result);
+    }
   }
 }
