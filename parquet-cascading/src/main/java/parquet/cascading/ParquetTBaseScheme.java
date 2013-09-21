e242085ac3f79d377ed62fd2620fbeaf9a61c969
Colin Marc <colinmarc@gmail.com>
2013-09-20 21:54:38 -0500

    add an empty constructor for ParquetTBaseScheme (which only works for reads)

--- parquet-cascading/src/main/java/parquet/cascading/ParquetTBaseScheme.java
+++ parquet-cascading/src/main/java/parquet/cascading/ParquetTBaseScheme.java
@@ -23,6 +23,7 @@ import org.apache.thrift.TBase;
 import parquet.hadoop.ParquetInputFormat;
 import parquet.hadoop.mapred.DeprecatedParquetInputFormat;
 import parquet.hadoop.mapred.DeprecatedParquetOutputFormat;
+import parquet.hadoop.thrift.ParquetThriftInputFormat;
 import parquet.hadoop.thrift.ThriftReadSupport;
 import parquet.hadoop.thrift.ThriftWriteSupport;
 import parquet.thrift.TBaseRecordConverter;
@@ -31,7 +32,11 @@ import cascading.tap.Tap;
 
 public class ParquetTBaseScheme<T extends TBase<?,?>> extends ParquetValueScheme<T> {
 
-  private final Class<T> thriftClass;
+  private Class<T> thriftClass;
+
+  // In the case of reads, we can read the thrift class from the file metadata
+  public ParquetTBaseScheme() {
+  }
 
   public ParquetTBaseScheme(Class<T> thriftClass) {
     this.thriftClass = thriftClass;
@@ -44,12 +49,21 @@ public class ParquetTBaseScheme<T extends TBase<?,?>> extends ParquetValueScheme
     jobConf.setInputFormat(DeprecatedParquetInputFormat.class);
     ParquetInputFormat.setReadSupportClass(jobConf, ThriftReadSupport.class);
     ThriftReadSupport.setRecordConverterClass(jobConf, TBaseRecordConverter.class);
+
+    if (thriftClass != null) {
+      ParquetThriftInputFormat.setThriftClass(jobConf, thriftClass);
+    }
   }
 
   @SuppressWarnings("rawtypes")
   @Override
   public void sinkConfInit(FlowProcess<JobConf> fp,
       Tap<JobConf, RecordReader, OutputCollector> tap, JobConf jobConf) {
+
+    if (thriftClass == null) {
+      throw new IllegalArgumentException("To use ParquetTBaseScheme as a sink, you must specify a thrift class in the constructor");
+    }
+
     jobConf.setOutputFormat(DeprecatedParquetOutputFormat.class);
     DeprecatedParquetOutputFormat.setWriteSupportClass(jobConf, ThriftWriteSupport.class);
     ThriftWriteSupport.<T>setThriftClass(jobConf, thriftClass);
