2fb1f7d79693fea18933092c8993be60b476f265
Tianshuo Deng <tdeng@twitter.com>
2013-10-02 16:09:59 -0700

    accept visitor

--- parquet-thrift/src/main/java/parquet/thrift/struct/CompatibilityChecker.java
+++ parquet-thrift/src/main/java/parquet/thrift/struct/CompatibilityChecker.java
@@ -3,20 +3,8 @@ package parquet.thrift.struct;
 public class CompatibilityChecker {
      public boolean areCompatible(ThriftType.StructType oldStruct, ThriftType.StructType newStruct){
 
-       for(ThriftField oldField:oldStruct.getChildren()){
-         short fieldId = oldField.getFieldId();
-         ThriftField newField= newStruct.getChildById(fieldId);
-         if (newField==null)
-           return false;
-         if (!newField.getType().equals(oldField.getType()))
-           return false;
-         if(!newField.getName().equals(oldField.getName()))
-           return false;
-         //TODO: check requirement
-         //TODO: recursivly, visitor pattern?
-       }
-       return true;
-     }
+       newStruct.accept(new CompatibleCheckerVisitor(oldStruct));
+       return true;// TODO return report
 
 }
 
