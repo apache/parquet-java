4e6863aaeb765f62322ed79bb7e82c930cb6e364
Tianshuo Deng <tdeng@twitter.com>
2013-10-02 14:18:52 -0700

    add checker

---
+++ parquet-thrift/src/main/java/parquet/thrift/struct/CompatibilityChecker.java
@@ -0,0 +1,7 @@
+package parquet.thrift.struct;
+
+public class CompatibilityChecker {
+     public boolean areCompatible(ThriftType.StructType oldStruct, ThriftType.StructType newStruct){
+       return true;
+     }
+}
