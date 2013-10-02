6d9d2b334db9db4896599f07e9a545961422db5f
Tianshuo Deng <tdeng@twitter.com>
2013-10-02 14:19:25 -0700

    add test

---
+++ parquet-thrift/src/test/java/parquet/thrift/struct/CompatibilityCheckerTest.java
@@ -0,0 +1,28 @@
+package parquet.thrift.struct;
+
+import org.codehaus.jackson.map.ObjectMapper;
+import org.junit.Test;
+import parquet.thrift.ThriftSchemaConverter;
+
+import java.io.File;
+
+import static junit.framework.Assert.assertEquals;
+
+
+public class CompatibilityCheckerTest {
+
+  @Test
+  public void testSaveJSON() throws Exception{
+    ThriftType.StructType expected = new ThriftSchemaConverter().toStructType(parquet.thrift.test.TestPersonWithRequiredPhone.class);
+    ObjectMapper mapper = new ObjectMapper();
+    mapper.writeValue(new File("oh_yeah.json"),expected);
+    ThriftType readResult=mapper.readValue(new File("oh_yeah.json"),ThriftType.StructType.class);
+    assertEquals(expected,readResult);
+  }
+  @Test
+  public void testReadJson() throws Exception{
+    ObjectMapper mapper = new ObjectMapper();
+    mapper.readValue(new File("oh_yeah.json"),ThriftType.StringType.class);
+
+  }
+}
