989e9dc13556213acf92758caaa7093679b4d52d
David Z. Chen <david.z.chen@outlook.com>
2013-10-03 11:57:59 -0700

    Plumb OriginalType through to ConvertedType in file in ParquetMetadataConverter.

--- parquet-hadoop/src/main/java/parquet/format/converter/ParquetMetadataConverter.java
+++ parquet-hadoop/src/main/java/parquet/format/converter/ParquetMetadataConverter.java
@@ -33,6 +33,7 @@ import java.util.Map.Entry;
 import java.util.Set;
 
 import parquet.Log;
+import parquet.format.ConvertedType;
 import parquet.format.ColumnChunk;
 import parquet.format.DataPageHeader;
 import parquet.format.DictionaryPageHeader;
@@ -53,6 +54,7 @@ import parquet.hadoop.metadata.ParquetMetadata;
 import parquet.io.ParquetDecodingException;
 import parquet.schema.GroupType;
 import parquet.schema.MessageType;
+import parquet.schema.OriginalType;
 import parquet.schema.PrimitiveType;
 import parquet.schema.PrimitiveType.PrimitiveTypeName;
 import parquet.schema.Type.Repetition;
@@ -99,6 +101,9 @@ public class ParquetMetadataConverter {
         SchemaElement element = new SchemaElement(primitiveType.getName());
         element.setRepetition_type(toParquetRepetition(primitiveType.getRepetition()));
         element.setType(getType(primitiveType.getPrimitiveTypeName()));
+        if (primitiveType.getOriginalType() != null) {
+          element.setConverted_type(getConvertedType(primitiveType.getOriginalType()));
+        }
         result.add(element);
       }
 
@@ -112,6 +117,9 @@ public class ParquetMetadataConverter {
       public void visit(GroupType groupType) {
         SchemaElement element = new SchemaElement(groupType.getName());
         element.setRepetition_type(toParquetRepetition(groupType.getRepetition()));
+        if (groupType.getOriginalType() != null) {
+          element.setConverted_type(getConvertedType(groupType.getOriginalType()));
+        }
         visitChildren(result, groupType, element);
       }
 
@@ -254,9 +262,43 @@ public class ParquetMetadataConverter {
       case FIXED_LEN_BYTE_ARRAY:
         return Type.FIXED_LEN_BYTE_ARRAY;
       default:
-        throw new RuntimeException("Unknown type " + type);
+        throw new RuntimeException("Unknown primitive type " + type);
     }
   }
+
+  OriginalType getOriginalType(ConvertedType type) {
+    switch (type) {
+      case UTF8:
+        return OriginalType.UTF8;
+      case MAP:
+        return OriginalType.MAP;
+      case MAP_KEY_VALUE:
+        return OriginalType.MAP_KEY_VALUE;
+      case LIST:
+        return OriginalType.LIST;
+      case ENUM:
+        return OriginalType.ENUM;
+      default:
+        throw new RuntimeException("Unknown converted type " + type);
+    }
+  }
+
+  ConvertedType getConvertedType(OriginalType type) {
+    switch (type) {
+      case UTF8:
+        return ConvertedType.UTF8;
+      case MAP:
+        return ConvertedType.MAP;
+      case MAP_KEY_VALUE:
+        return ConvertedType.MAP_KEY_VALUE;
+      case LIST:
+        return ConvertedType.LIST;
+      case ENUM:
+        return ConvertedType.ENUM;
+      default:
+        throw new RuntimeException("Unknown original type " + type);
+     }
+   }
 
   private void addKeyValue(FileMetaData fileMetaData, String key, String value) {
     KeyValue keyValue = new KeyValue(key);
@@ -341,15 +383,23 @@ public class ParquetMetadataConverter {
       }
       Repetition repetition = fromParquetRepetition(schemaElement.getRepetition_type());
       String name = schemaElement.getName();
+      OriginalType originalType = null;
+      if (schemaElement.isSetConverted_type()) {
+        originalType = getOriginalType(schemaElement.converted_type);
+      }
+
+      // Create Parquet Type.
       if (schemaElement.type != null) {
         result[i] = new PrimitiveType(
             repetition,
             getPrimitive(schemaElement.getType()),
-            name);
+            name,
+            originalType);
       } else {
         result[i] = new GroupType(
             repetition,
             name,
+            originalType,
             convertChildren(schema, schemaElement.getNum_children()));
       }
     }
