942cfe26cceb69cc420e5019f854213c654ec844
Lukas <lukas.nalezenec@gmail.com>
2014-01-19 00:31:15 +0100

    Dictionary enum conversion

--- parquet-protobuf/src/main/java/parquet/proto/converters/ProtoEnumConverter.java
+++ parquet-protobuf/src/main/java/parquet/proto/converters/ProtoEnumConverter.java
@@ -17,6 +17,8 @@
 package parquet.proto.converters;
 
 import com.google.protobuf.Descriptors;
+import parquet.column.Dictionary;
+import parquet.io.InvalidRecordException;
 import parquet.io.api.Binary;
 import parquet.io.api.PrimitiveConverter;
 
@@ -29,6 +31,7 @@ final class ProtoEnumConverter extends PrimitiveConverter {
 
   private final Descriptors.FieldDescriptor fieldType;
   private final Map<Binary, Descriptors.EnumValueDescriptor> enumLookup;
+  private Descriptors.EnumValueDescriptor[] dict;
   private final ParentValueContainer parent;
 
   public ProtoEnumConverter(ParentValueContainer parent, Descriptors.FieldDescriptor fieldType) {
@@ -37,7 +40,10 @@ final class ProtoEnumConverter extends PrimitiveConverter {
     this.enumLookup = makeLookupStructure(fieldType);
   }
 
-  Map<Binary, Descriptors.EnumValueDescriptor> makeLookupStructure(Descriptors.FieldDescriptor enumFieldType) {
+  /**
+   * Fills lookup structure for translating between parquet enum values and Protocol buffer enum values.
+   * */
+  private Map<Binary, Descriptors.EnumValueDescriptor> makeLookupStructure(Descriptors.FieldDescriptor enumFieldType) {
     Descriptors.EnumDescriptor enumType = enumFieldType.getEnumType();
     Map<Binary, Descriptors.EnumValueDescriptor> lookupStructure = new HashMap<Binary, Descriptors.EnumValueDescriptor>();
 
@@ -51,19 +57,46 @@ final class ProtoEnumConverter extends PrimitiveConverter {
     return lookupStructure;
   }
 
-  @Override
-  final public void addBinary(Binary binaryValue) {
+  /**
+   * Translates given parquet enum value to protocol buffer enum value.
+   * @throws InvalidRecordException is there is no corresponding value.
+   * */
+  private Descriptors.EnumValueDescriptor translateEnumValue(Binary binaryValue) {
     Descriptors.EnumValueDescriptor protoValue = enumLookup.get(binaryValue);
 
     if (protoValue == null) {
       Set<Binary> knownValues = enumLookup.keySet();
       String msg = "Illegal enum value \"" + binaryValue + "\""
-         + " in protocol buffer \"" + fieldType.getFullName() + "\""
-         + " legal values are: \"" + knownValues + "\"";
-      throw new RuntimeException(msg);
+              + " in protocol buffer \"" + fieldType.getFullName() + "\""
+              + " legal values are: \"" + knownValues + "\"";
+      throw new InvalidRecordException(msg);
     }
+    return protoValue;
+  }
 
+  @Override
+  final public void addBinary(Binary binaryValue) {
+    Descriptors.EnumValueDescriptor protoValue = translateEnumValue(binaryValue);
     parent.add(protoValue);
+  }
+
+  @Override
+  public void addValueFromDictionary(int dictionaryId) {
+    parent.add(dict[dictionaryId]);
+  }
+
+  @Override
+  public boolean hasDictionarySupport() {
+    return true;
+  }
+
+  @Override
+  public void setDictionary(Dictionary dictionary) {
+    dict = new  Descriptors.EnumValueDescriptor[dictionary.getMaxId() + 1];
+    for (int i = 0; i <= dictionary.getMaxId(); i++) {
+      Binary binaryValue = dictionary.decodeToBinary(i);
+      dict[i] = translateEnumValue(binaryValue);
+    }
   }
 
 }
