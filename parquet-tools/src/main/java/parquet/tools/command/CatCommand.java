8cc8bdc05260d0f5f60b3435da7d0f0edef6a786
Wesley Peck <wesley.peck@arrisi.com>
2014-02-18 13:28:31 -0600

    Merge the parquet-tools project into parquet-mr.

---
+++ parquet-tools/src/main/java/parquet/tools/command/CatCommand.java
@@ -0,0 +1,67 @@
+/**
+ * Copyright 2013 ARRIS, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package parquet.tools.command;
+
+import java.io.PrintWriter;
+
+import org.apache.commons.cli.CommandLine;
+import org.apache.hadoop.fs.Path;
+
+import parquet.hadoop.ParquetReader;
+import parquet.tools.Main;
+import parquet.tools.read.SimpleReadSupport;
+import parquet.tools.read.SimpleRecord;
+
+public class CatCommand extends ArgsOnlyCommand {
+  public static final String[] USAGE = new String[] {
+    "<input>",
+    "where <input> is the parquet file to print to stdout"
+  };
+
+  public CatCommand() {
+    super(1, 1);
+  }
+
+  @Override
+  public String[] getUsageDescription() {
+    return USAGE;
+  }
+
+  @Override
+  public void execute(CommandLine options) throws Exception {
+    super.execute(options);
+
+    String[] args = options.getArgs();
+    String input = args[0];
+
+    ParquetReader<SimpleRecord> reader = null;
+    try {
+      PrintWriter writer = new PrintWriter(Main.out, true);
+      reader = new ParquetReader<SimpleRecord>(new Path(input), new SimpleReadSupport());
+      for (SimpleRecord value = reader.read(); value != null; value = reader.read()) {
+        value.prettyPrint(writer);
+        writer.println();
+      }
+    } finally {
+      if (reader != null) {
+        try {
+          reader.close();
+        } catch (Exception ex) {
+        }
+      }
+    }
+  }
+}
