238cbbea342871a6f35a1f26820d0005369cbb4b
Alex Levenson <alexlevenson@twitter.com>
2014-06-16 15:30:05 -0700

    Add scala dsl

---
+++ parquet-scala/src/main/scala/parquet/filter2/FilterPredicateDsl.scala
@@ -0,0 +1,26 @@
+package parquet.filter2
+
+object FilterPredicateDsl {
+
+  case class Column[T](columnPath: String) {
+    private[this] val column = FilterPredicates.column[T](columnPath)
+    def === (v: T) = FilterPredicates.eq(column, v)
+    def !== (v: T) = FilterPredicates.notEq(column, v)
+    def >(v: T) = FilterPredicates.gt(column, v)
+    def >=(v: T) = FilterPredicates.gtEq(column, v)
+    def <(v: T) = FilterPredicates.lt(column, v)
+    def <=(v: T) = FilterPredicates.ltEq(column, v)
+
+    override def equals(x: Any) =
+      throw new UnsupportedOperationException("You probably meant to use === or !==")
+  }
+
+  implicit def enrich(pred: FilterPredicate): RichPredicate = new RichPredicate(pred)
+
+  class RichPredicate(val pred: FilterPredicate) {
+    def &&(other: FilterPredicate) = FilterPredicates.and(pred, other)
+    def ||(other: FilterPredicate) = FilterPredicates.or(pred, other)
+    def unary_! = FilterPredicates.not(pred)
+  }
+
+}
