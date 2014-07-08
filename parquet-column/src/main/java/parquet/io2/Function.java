NathanHowell/incubator-parquet-mr

Support lifted coercions of file schemas into compatible read schemas.
Pig and Hive assume certain values are nullable but should support
reading from Parquet files that do not have a representation of null
values. This patch introduces the notion type lifting. Lifted values
exist at runtime merely to provide a translation between the disk schema
and requested schema.

For example, a Parquet file with the following schema can not be read
from Pig or Hive:

```
message file_schema {
  optional binary key (UTF8);
  repeated group records {
    optional binary name (UTF8);
    optional binary type;
  }
}
```

The requested schema that e.g. Pig requires wraps the `records` group
in an optional group in order to provide an explicit representation
of a null value.

```
message requested_schema {
  optional binary key (UTF8);
  optional group records (LIST) {
    repeated group records {
      optional binary name (UTF8);
      optional binary type;
    }
  }
}
```

The file and requested schemas are not equal but are compatible for
reads with a simple transformation. When a nullable field present
in the requested schema but not on the file schema a GroupType wrapper
is inserted. This lifts non-nullable values into a nullable value.

parquet-column/src/main/java/parquet/io/Function.java
@@ -1,5 +1,5 @@
 /**
- * Copyright 2012 Twitter, Inc.
+ * Copyright 2014 GoDaddy, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,30 +13,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package parquet.schema;
+package parquet.io;
 
-/**
- * Implement this interface to visit a schema
- *
- * type.accept(new TypeVisitor() { ... });
- *
- * @author Julien Le Dem
- *
- */
-public interface TypeVisitor<T> {
-
-  /**
-   * @param groupType the group type to visit
-   */
-  T visit(GroupType groupType);
-
-  /**
-   * @param messageType the message type to visit
-   */
-  T visit(MessageType messageType);
-
-  /**
-   * @param primitiveType the primitive type to visit
-   */
-  T visit(PrimitiveType primitiveType);
+public interface Function<T, R> {
+  R call(final T value);
 }

parquet-column/src/main/java/parquet/io/LeafInfo.java
@@ -1,0 +1,52 @@
+/**
+ * Copyright 2014 GoDaddy, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package parquet.io;
+
+final class LeafInfo {
+  private final ColumnPath physicalPath;
+  private final ColumnPath logicalPath;
+
+  static final LeafInfo empty = new LeafInfo(ColumnPath.empty, ColumnPath.empty);
+
+  LeafInfo(
+      final ColumnPath physicalPath,
+      final ColumnPath logicalPath) {
+    this.physicalPath = physicalPath;
+    this.logicalPath = logicalPath;
+  }
+
+  LeafInfo combine(final LeafInfo other) {
+    return new LeafInfo(
+        this.physicalPath.combine(other.physicalPath),
+        this.logicalPath.combine(other.logicalPath));
+  }
+
+  public ColumnPath getPhysicalPath() {
+    return physicalPath;
+  }
+
+  public ColumnPath getLogicalPath() {
+    return logicalPath;
+  }
+
+  @Override
+  public String toString() {
+    return "LeafInfo{" +
+        "physicalPath=" + physicalPath +
+        ", logicalPath=" + logicalPath +
+        '}';
+  }
+}

parquet-column/src/main/java/parquet/io/Option.java
@@ -1,0 +1,115 @@
+/**
+ * Copyright 2014 GoDaddy, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package parquet.io;
+
+public abstract class Option<T> {
+  Option() {
+  }
+
+  private static class Some<T> extends Option<T> {
+    private final T value;
+
+    public Some(final T value) {
+      this.value = value;
+    }
+
+    @Override
+    public <R> Option<R> map(final Function<T, R> f) {
+      return new Some<R>(f.call(this.value));
+    }
+
+    @Override
+    public <R> Option<R> flatMap(final Function<T, Option<R>> f) {
+      return f.call(this.value);
+    }
+
+    @Override
+    public Option<T> or(final Function<Void, Option<T>> alternative) {
+      return this;
+    }
+
+    @Override
+    public <R> Option<R> ap(final Option<Function<T, R>> f) {
+      return f.map(new Function<Function<T, R>, R>() {
+        @Override
+        public R call(final Function<T, R> g) {
+          return g.call(value);
+        }
+      });
+    }
+
+    @Override
+    public T get() {
+      return value;
+    }
+
+    @Override
+    public String toString() {
+      return "Some{" +
+          "value=" + value +
+          '}';
+    }
+  }
+
+  private final static class None<T> extends Option<T> {
+    public None() {
+    }
+
+    @Override
+    public <R> Option<R> map(final Function<T, R> f) {
+      return new None<R>();
+    }
+
+    @Override
+    public <R> Option<R> flatMap(final Function<T, Option<R>> f) {
+      return new None<R>();
+    }
+
+    @Override
+    public Option<T> or(final Function<Void, Option<T>> alternative) {
+      return alternative.call(null);
+    }
+
+    @Override
+    public <R> Option<R> ap(final Option<Function<T, R>> f) {
+      return new None<R>();
+    }
+
+    @Override
+    public T get() {
+      throw new RuntimeException();
+    }
+
+    @Override
+    public String toString() {
+      return "None";
+    }
+  }
+
+  public abstract <R> Option<R> map(final Function<T, R> f);
+  public abstract <R> Option<R> flatMap(final Function<T, Option<R>> f);
+  public abstract Option<T> or(final Function<Void, Option<T>> alternative);
+  public abstract <R> Option<R> ap(final Option<Function<T, R>> f);
+  public abstract T get();
+
+  public static <T> Option<T> pure(final T value) {
+    return new Some<T>(value);
+  }
+
+  public static <T> Option<T> empty() {
+    return new None<T>();
+  }
+}

apache/parquet-java

parquet-column/src/main/java/parquet/io2/Function.java
@@ -1,5 +1,5 @@
 /**
- * Copyright 2012 Twitter, Inc.
+ * Copyright 2014 GoDaddy, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,30 +13,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package parquet.schema;
+package parquet.io2;
 
-/**
- * Implement this interface to visit a schema
- *
- * type.accept(new TypeVisitor() { ... });
- *
- * @author Julien Le Dem
- *
- */
-public interface TypeVisitor<T> {
-
-  /**
-   * @param groupType the group type to visit
-   */
-  T visit(GroupType groupType);
-
-  /**
-   * @param messageType the message type to visit
-   */
-  T visit(MessageType messageType);
-
-  /**
-   * @param primitiveType the primitive type to visit
-   */
-  T visit(PrimitiveType primitiveType);
+public interface Function<T, R> {
+  R call(final T value);
 }

parquet-column/src/main/java/parquet/io2/LeafInfo.java
@@ -1,0 +1,52 @@
+/**
+ * Copyright 2014 GoDaddy, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package parquet.io2;
+
+final class LeafInfo {
+  private final ColumnPath physicalPath;
+  private final ColumnPath logicalPath;
+
+  static final LeafInfo empty = new LeafInfo(ColumnPath.empty, ColumnPath.empty);
+
+  LeafInfo(
+      final ColumnPath physicalPath,
+      final ColumnPath logicalPath) {
+    this.physicalPath = physicalPath;
+    this.logicalPath = logicalPath;
+  }
+
+  LeafInfo combine(final LeafInfo other) {
+    return new LeafInfo(
+        this.physicalPath.combine(other.physicalPath),
+        this.logicalPath.combine(other.logicalPath));
+  }
+
+  public ColumnPath getPhysicalPath() {
+    return physicalPath;
+  }
+
+  public ColumnPath getLogicalPath() {
+    return logicalPath;
+  }
+
+  @Override
+  public String toString() {
+    return "LeafInfo{" +
+        "physicalPath=" + physicalPath +
+        ", logicalPath=" + logicalPath +
+        '}';
+  }
+}

parquet-column/src/main/java/parquet/io2/Option.java
@@ -1,0 +1,115 @@
+/**
+ * Copyright 2014 GoDaddy, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package parquet.io2;
+
+public abstract class Option<T> {
+  Option() {
+  }
+
+  private static class Some<T> extends Option<T> {
+    private final T value;
+
+    public Some(final T value) {
+      this.value = value;
+    }
+
+    @Override
+    public <R> Option<R> map(final Function<T, R> f) {
+      return new Some<R>(f.call(this.value));
+    }
+
+    @Override
+    public <R> Option<R> flatMap(final Function<T, Option<R>> f) {
+      return f.call(this.value);
+    }
+
+    @Override
+    public Option<T> or(final Function<Void, Option<T>> alternative) {
+      return this;
+    }
+
+    @Override
+    public <R> Option<R> ap(final Option<Function<T, R>> f) {
+      return f.map(new Function<Function<T, R>, R>() {
+        @Override
+        public R call(final Function<T, R> g) {
+          return g.call(value);
+        }
+      });
+    }
+
+    @Override
+    public T get() {
+      return value;
+    }
+
+    @Override
+    public String toString() {
+      return "Some{" +
+          "value=" + value +
+          '}';
+    }
+  }
+
+  private final static class None<T> extends Option<T> {
+    public None() {
+    }
+
+    @Override
+    public <R> Option<R> map(final Function<T, R> f) {
+      return new None<R>();
+    }
+
+    @Override
+    public <R> Option<R> flatMap(final Function<T, Option<R>> f) {
+      return new None<R>();
+    }
+
+    @Override
+    public Option<T> or(final Function<Void, Option<T>> alternative) {
+      return alternative.call(null);
+    }
+
+    @Override
+    public <R> Option<R> ap(final Option<Function<T, R>> f) {
+      return new None<R>();
+    }
+
+    @Override
+    public T get() {
+      throw new RuntimeException();
+    }
+
+    @Override
+    public String toString() {
+      return "None";
+    }
+  }
+
+  public abstract <R> Option<R> map(final Function<T, R> f);
+  public abstract <R> Option<R> flatMap(final Function<T, Option<R>> f);
+  public abstract Option<T> or(final Function<Void, Option<T>> alternative);
+  public abstract <R> Option<R> ap(final Option<Function<T, R>> f);
+  public abstract T get();
+
+  public static <T> Option<T> pure(final T value) {
+    return new Some<T>(value);
+  }
+
+  public static <T> Option<T> empty() {
+    return new None<T>();
+  }
+}