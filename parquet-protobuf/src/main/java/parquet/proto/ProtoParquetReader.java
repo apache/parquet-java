9c8fcd5f790e0194d5c53277b4f2e3da22db4b1b
Viktor Szathmáry <phraktle@gmail.com>
2014-09-23 00:06:16 +0200

    make ProtoParquetReader#builder static

--- parquet-protobuf/src/main/java/parquet/proto/ProtoParquetReader.java
+++ parquet-protobuf/src/main/java/parquet/proto/ProtoParquetReader.java
@@ -30,7 +30,7 @@ import parquet.hadoop.ParquetReader;
 public class ProtoParquetReader<T extends MessageOrBuilder> extends ParquetReader<T> {
 
   @SuppressWarnings("unchecked")
-  public Builder<T> builder(Path file) {
+  public static Builder<T> builder(Path file) {
     return ParquetReader.builder(new ProtoReadSupport(), file);
   }
 
