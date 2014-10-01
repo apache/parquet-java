da9129927bce90feb6d2860745263f4d74d0dfa8
Ryan Blue <rblue@cloudera.com>
2014-10-01 13:44:45 -0700

    PARQUET-64: Add new OriginalTypes in parquet-format 2.2.0.
    This implements the restrictions for those types documented in the parquet-format logical types spec.
    
    This requires a release of parquet-format 2.2.0 with the new types. I'll rebase and update the dependency when it is released.
    
    Author: Ryan Blue <rblue@cloudera.com>
    
    Closes #31 from rdblue/PARQUET-64-add-new-types and squashes the following commits:
    
    10feab9 [Ryan Blue] PARQUET-64: Add new OriginalTypes in parquet-format 2.2.0.

--- parquet-column/src/main/java/parquet/schema/OriginalType.java
+++ parquet-column/src/main/java/parquet/schema/OriginalType.java
@@ -21,5 +21,19 @@ public enum OriginalType {
   UTF8,
   MAP_KEY_VALUE,
   ENUM,
-  DECIMAL;
+  DECIMAL,
+  DATE,
+  TIME_MILLIS,
+  TIMESTAMP_MILLIS,
+  UINT_8,
+  UINT_16,
+  UINT_32,
+  UINT_64,
+  INT_8,
+  INT_16,
+  INT_32,
+  INT_64,
+  JSON,
+  BSON,
+  INTERVAL;
 }
