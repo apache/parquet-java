parthchandra/incubator-parquet-mr

Use ByteBuffers in the Write path. Allow callers to pass in an allocator to allocate the ByteBuffer.

parquet-column/src/main/java/parquet/column/values/rle/RunLengthBitPackingHybridValuesWriter.java
@@ -17,8 +17,10 @@ package parquet.column.values.rle;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.nio.ByteBuffer;
 
 import parquet.Ints;
+import parquet.bytes.ByteBufferAllocator;
 import parquet.bytes.BytesInput;
 import parquet.bytes.BytesUtils;
 import parquet.column.Encoding;
@@ -31,9 +33,11 @@ import parquet.io.ParquetEncodingException;
 public class RunLengthBitPackingHybridValuesWriter extends ValuesWriter {
   private final RunLengthBitPackingHybridEncoder encoder;
   private final ByteArrayOutputStream length;
+  private ByteBufferAllocator allocator;
 
-  public RunLengthBitPackingHybridValuesWriter(int bitWidth, int initialCapacity) {
-    this.encoder = new RunLengthBitPackingHybridEncoder(bitWidth, initialCapacity);
+  public RunLengthBitPackingHybridValuesWriter(int bitWidth, int initialCapacity, ByteBufferAllocator allocator) {
+    this.allocator=allocator;
+    this.encoder = new RunLengthBitPackingHybridEncoder(bitWidth, initialCapacity, this.allocator);
     this.length = new ByteArrayOutputStream(4);
   }
 
@@ -82,6 +86,16 @@ public class RunLengthBitPackingHybridValuesWriter extends ValuesWriter {
   public void reset() {
     encoder.reset();
     length.reset();
+  }
+
+  @Override
+  public void close() {
+    encoder.close();
+    try {
+      length.close();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
   }
 
   @Override

apache/parquet-java

Conflicts:
	parquet-column/src/main/java/parquet/column/ColumnWriteStore.java
	parquet-column/src/main/java/parquet/column/ColumnWriter.java
	parquet-column/src/main/java/parquet/column/ParquetProperties.java
	parquet-column/src/main/java/parquet/column/impl/ColumnWriteStoreV1.java
	parquet-column/src/main/java/parquet/column/impl/ColumnWriterV1.java
	parquet-column/src/main/java/parquet/column/values/dictionary/DictionaryValuesWriter.java
	parquet-column/src/main/java/parquet/column/values/rle/RunLengthBitPackingHybridValuesWriter.java
	parquet-column/src/test/java/parquet/column/values/dictionary/TestDictionary.java
	parquet-column/src/test/java/parquet/io/TestColumnIO.java
	parquet-hadoop/src/main/java/parquet/hadoop/ColumnChunkPageWriteStore.java
	parquet-hadoop/src/main/java/parquet/hadoop/InternalParquetRecordWriter.java
	parquet-hadoop/src/main/java/parquet/hadoop/ParquetRecordWriter.java
	parquet-hadoop/src/test/java/parquet/hadoop/TestParquetFileWriter.java

parquet-column/src/main/java/parquet/column/values/rle/RunLengthBitPackingHybridValuesWriter.java
@@ -18,9 +18,11 @@
  */
 package parquet.column.values.rle;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 
 import parquet.Ints;
+import parquet.bytes.ByteBufferAllocator;
 import parquet.bytes.BytesInput;
 import parquet.column.Encoding;
 import parquet.column.values.ValuesWriter;
@@ -31,9 +33,13 @@ import parquet.io.ParquetEncodingException;
  */
 public class RunLengthBitPackingHybridValuesWriter extends ValuesWriter {
   private final RunLengthBitPackingHybridEncoder encoder;
+  private final ByteArrayOutputStream length;
+  private ByteBufferAllocator allocator;
 
-  public RunLengthBitPackingHybridValuesWriter(int bitWidth, int initialCapacity) {
-    this.encoder = new RunLengthBitPackingHybridEncoder(bitWidth, initialCapacity);
+  public RunLengthBitPackingHybridValuesWriter(int bitWidth, int initialCapacity, ByteBufferAllocator allocator) {
+    this.allocator=allocator;
+    this.encoder = new RunLengthBitPackingHybridEncoder(bitWidth, initialCapacity, this.allocator);
+    this.length = new ByteArrayOutputStream(4);
   }
 
   @Override
@@ -79,6 +85,16 @@ public class RunLengthBitPackingHybridValuesWriter extends ValuesWriter {
   @Override
   public void reset() {
     encoder.reset();
+  }
+
+  @Override
+  public void close() {
+    encoder.close();
+    try {
+      length.close();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
   }
 
   @Override