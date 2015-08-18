bdc8fdf7a43317815d6df8258d5948b58827c807
Nezih Yigitbasi <nyigitbasi@netflix.com>
2015-08-18 00:38:22 -0700

    Handle all map key types with cat tool's json dump

--- parquet-tools/src/main/java/org/apache/parquet/tools/read/SimpleMapRecord.java
+++ parquet-tools/src/main/java/org/apache/parquet/tools/read/SimpleMapRecord.java
@@ -20,6 +20,7 @@ package org.apache.parquet.tools.read;
 
 import com.google.common.collect.Maps;
 
+import java.util.Arrays;
 import java.util.Map;
 
 public class SimpleMapRecord extends SimpleRecord {
@@ -30,14 +31,52 @@ public class SimpleMapRecord extends SimpleRecord {
       String key = null;
       Object val = null;
       for (NameValue kv : ((SimpleRecord) value.getValue()).values) {
-        if (kv.getName().equals("key")) {
-          key = (String) kv.getValue();
-        } else if (kv.getName().equals("value")) {
-          val = toJsonValue(kv.getValue());
+        String kvName = kv.getName();
+        Object kvValue = kv.getValue();
+        if (kvName.equals("key")) {
+          key = keyToString(kvValue);
+        } else if (kvName.equals("value")) {
+          val = toJsonValue(kvValue);
         }
       }
       result.put(key, val);
     }
     return result;
+  }
+
+  private String keyToString(Object kvValue) {
+    Class<?> type = kvValue.getClass();
+    if (type.isArray()) {
+
+      if (type.getComponentType() == boolean.class) {
+        return Arrays.toString((boolean[]) kvValue);
+      }
+      else if (type.getComponentType() == byte.class) {
+        return Arrays.toString((byte[]) kvValue);
+      }
+      else if (type.getComponentType() == char.class) {
+        return Arrays.toString((char[]) kvValue);
+      }
+      else if (type.getComponentType() == double.class) {
+        return Arrays.toString((double[]) kvValue);
+      }
+      else if (type.getComponentType() == float.class) {
+        return Arrays.toString((float[]) kvValue);
+      }
+      else if (type.getComponentType() == int.class) {
+        return Arrays.toString((int[]) kvValue);
+      }
+      else if (type.getComponentType() == long.class) {
+        return Arrays.toString((long[]) kvValue);
+      }
+      else if (type.getComponentType() == short.class) {
+        return Arrays.toString((short[]) kvValue);
+      }
+      else {
+        return Arrays.toString((Object[]) kvValue);
+      }
+    } else {
+      return String.valueOf(kvValue);
+    }
   }
 }
