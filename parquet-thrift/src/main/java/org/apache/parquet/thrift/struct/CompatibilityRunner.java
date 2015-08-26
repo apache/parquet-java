1f22bf7c6ceae78f865700da201bf726778ac853
Alex Levenson <alexlevenson@twitter.com>
2015-08-26 16:26:09 -0700

    Add a flag to allow empty structs in the compat checker

--- parquet-thrift/src/main/java/org/apache/parquet/thrift/struct/CompatibilityRunner.java
+++ parquet-thrift/src/main/java/org/apache/parquet/thrift/struct/CompatibilityRunner.java
@@ -57,6 +57,7 @@ public class CompatibilityRunner {
   private static void compareJson(LinkedList<String> arguments) throws IOException {
     String oldJsonPath = arguments.pollFirst();
     String newJsonPath = arguments.pollFirst();
+    boolean allowEmptyStruct = "--allow-empty-struct".equals(arguments.pollFirst());
 
     File oldJsonFile = new File(oldJsonPath);
     checkExist(oldJsonFile);
@@ -75,9 +76,14 @@ public class CompatibilityRunner {
     }
 
     if (report.hasEmptyStruct()) {
-      System.err.println("schema contains empty struct");
-      System.err.println(report.getMessages());
-      System.exit(1);
+      if (allowEmptyStruct) {
+        System.err.println("WARNING: schema contains empty struct, but allowing because --allow-empty-struct was passed");
+        System.err.println(report.getMessages());
+      } else {
+        System.err.println("schema contains empty struct");
+        System.err.println(report.getMessages());
+        System.exit(1);
+      }
     }
 
     System.out.println("[success] schema is compatible");
