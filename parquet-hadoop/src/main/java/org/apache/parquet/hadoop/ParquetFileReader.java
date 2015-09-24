cb985526fbd981e67117b7e2ea6e8f283f61d10b
Ryan Blue <blue@apache.org>
2015-09-24 10:03:34 -0700

    PARQUET-382: Add methods to append encoded data to files.
    This allows appending encoded data blocks to open ParquetFileWriters,
    which makes it possible to merge multiple Parquet files without
    re-encoding all of the records.
    
    This works by finding the column chunk for each column in the file
    schema and then streaming the encoded data from one file to the other.
    New starting offsets are tracked and the column chunk metadata in the
    footer is updated with the new starting positions.

--- parquet-hadoop/src/main/java/org/apache/parquet/hadoop/ParquetFileReader.java
+++ parquet-hadoop/src/main/java/org/apache/parquet/hadoop/ParquetFileReader.java
@@ -443,6 +443,12 @@ public class ParquetFileReader implements Closeable {
     }
   }
 
+  static ParquetFileReader open(Configuration conf, Path file) throws IOException {
+    ParquetMetadata footer = readFooter(conf, file, NO_FILTER);
+    return new ParquetFileReader(conf, footer.getFileMetaData(), file,
+        footer.getBlocks(), footer.getFileMetaData().getSchema().getColumns());
+  }
+
   private final CodecFactory codecFactory;
   private final List<BlockMetaData> blocks;
   private final FSDataInputStream f;
@@ -483,6 +489,9 @@ public class ParquetFileReader implements Closeable {
     this.codecFactory = new CodecFactory(configuration);
   }
 
+  public void appendTo(ParquetFileWriter writer) throws IOException {
+    writer.appendRowGroups(f, blocks, true);
+  }
 
   /**
    * Reads all the columns requested from the row group at the current file position.
