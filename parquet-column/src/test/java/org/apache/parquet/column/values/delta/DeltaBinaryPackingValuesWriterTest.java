lunchev/incubator-parquet-mr

INT64 support for Delta Encoding

parquet-column/src/test/java/org/apache/parquet/column/values/delta/DeltaBinaryPackingValuesWriterTest.java
@@ -139,6 +139,19 @@ public class DeltaBinaryPackingValuesWriterTest {
   }
 
   @Test
+  public void shouldReadMaxMinValueLong() throws IOException {
+    long[] data = new long[10];
+    for (int i = 0; i < data.length; i++) {
+      if(i%2==0) {
+        data[i]=Long.MIN_VALUE;
+      }else {
+        data[i]=Long.MAX_VALUE;
+      }
+    }
+    shouldReadAndWriteLong(data, data.length);
+  }
+
+  @Test
   public void shouldReturnCorrectOffsetAfterInitialization() throws IOException {
     int[] data = new int[2 * blockSize + 3];
     for (int i = 0; i < data.length; i++) {
@@ -209,6 +222,22 @@ public class DeltaBinaryPackingValuesWriterTest {
   }
 
   @Test
+  public void randomDataTestLong() throws IOException {
+    int maxSize = 1000;
+    long[] data = new long[maxSize];
+
+    for (int round = 0; round < 100000; round++) {
+      int size = random.nextInt(maxSize);
+
+      for (int i = 0; i < size; i++) {
+        data[i] = random.nextLong();
+      }
+      shouldReadAndWriteLong(data, size);
+      writer.reset();
+    }
+  }
+
+  @Test
   public void randomDataTest() throws IOException {
     int maxSize = 1000;
     int[] data = new int[maxSize];
@@ -228,6 +257,13 @@ public class DeltaBinaryPackingValuesWriterTest {
 
   private void shouldWriteAndRead(int[] data) throws IOException {
     shouldReadAndWrite(data, data.length);
+    writer.reset();
+    
+    long[] dataInLong = new long[data.length]; 
+    for (int i = 0; i < data.length; i++) {
+      dataInLong[i] = (long) data[i];
+    }
+    shouldReadAndWriteLong(dataInLong, data.length);
   }
 
   private void shouldReadAndWrite(int[] data, int length) throws IOException {
@@ -257,6 +293,36 @@ public class DeltaBinaryPackingValuesWriterTest {
   private void writeData(int[] data, int length) {
     for (int i = 0; i < length; i++) {
       writer.writeInteger(data[i]);
+    }
+  }
+
+  private void shouldReadAndWriteLong(long[] data, int length) throws IOException {
+    writeDataLong(data, length);
+    reader = new DeltaBinaryPackingValuesReader();
+    byte[] page = writer.getBytes().toByteArray();
+    int miniBlockSize = blockSize / miniBlockNum;
+
+    double miniBlockFlushed = Math.ceil(((double) length - 1) / miniBlockSize);
+    double blockFlushed = Math.ceil(((double) length - 1) / blockSize);
+    double estimatedSize = 3 * 5 + 1 * 10 //blockHeader, 3 * int + 1 * long
+        + 8 * miniBlockFlushed * miniBlockSize //data(aligned to miniBlock)
+        + blockFlushed * miniBlockNum //bitWidth of mini blocks
+        + (10.0 * blockFlushed);//min delta for each block
+    assertTrue(estimatedSize >= page.length);
+    reader.initFromPage(100, page, 0);
+
+    for (int i = 0; i < length; i++) {
+      assertEquals(data[i], reader.readLong());
+    }
+  }
+
+  private void writeDataLong(long[] data) {
+    writeDataLong(data, data.length);
+  }
+
+  private void writeDataLong(long[] data, int length) {
+    for (int i = 0; i < length; i++) {
+      writer.writeLong(data[i]);
     }
   }
 }

parquet-encoding/src/test/java/org/apache/parquet/column/values/bitpacking/TestByteBitPacking.java
@@ -45,6 +45,24 @@ public class TestByteBitPacking {
       Assert.assertArrayEquals("width "+i, values, unpacked);
     }
   }
+  
+  @Test
+  public void testPackUnPackLong() {
+    LOG.debug("");
+    LOG.debug("testPackUnPackLong");
+    for (int i = 1; i < 64; i++) {
+      LOG.debug("Width: " + i);
+      long[] unpacked32 = new long[32];
+      long[] unpacked8 = new long[32];
+      long[] values = generateValuesLong(i);
+      packUnpack32(Packer.BIG_ENDIAN.newBytePacker(i), values, unpacked32);
+      LOG.debug("Output 32: " + TestBitPacking.toString(unpacked32));
+      Assert.assertArrayEquals("width "+i, values, unpacked32);
+      packUnpack8(Packer.BIG_ENDIAN.newBytePacker(i), values, unpacked8);
+      LOG.debug("Output 8: " + TestBitPacking.toString(unpacked8));
+      Assert.assertArrayEquals("width "+i, values, unpacked8);
+    }
+  }
 
   private void packUnpack(BytePacker packer, int[] values, int[] unpacked) {
     byte[] packed = new byte[packer.getBitWidth() * 4];
@@ -53,10 +71,38 @@ public class TestByteBitPacking {
     packer.unpack32Values(packed, 0, unpacked, 0);
   }
 
+  private void packUnpack32(BytePacker packer, long[] values, long[] unpacked) {
+    byte[] packed = new byte[packer.getBitWidth() * 4];
+    packer.pack32Values(values, 0, packed, 0);
+    LOG.debug("packed: " + TestBitPacking.toString(packed));
+    packer.unpack32Values(packed, 0, unpacked, 0);
+  }
+
+  private void packUnpack8(BytePacker packer, long[] values, long[] unpacked) {
+    byte[] packed = new byte[packer.getBitWidth() * 4];
+    for (int i = 0; i < 4; i++) {
+      packer.pack8Values(values,  8 * i, packed, packer.getBitWidth() * i);
+    }
+    LOG.debug("packed: " + TestBitPacking.toString(packed));
+    for (int i = 0; i < 4; i++) {
+      packer.unpack8Values(packed, packer.getBitWidth() * i, unpacked, 8 * i);
+    }
+  }
+
   private int[] generateValues(int bitWidth) {
     int[] values = new int[32];
     for (int j = 0; j < values.length; j++) {
       values[j] = (int)(Math.random() * 100000) % (int)Math.pow(2, bitWidth);
+    }
+    LOG.debug("Input:  " + TestBitPacking.toString(values));
+    return values;
+  }
+
+  private long[] generateValuesLong(int bitWidth) {
+    long[] values = new long[32];
+    for (int j = 0; j < values.length; j++) {
+      values[j] = ((long)(Math.random() * (1l<<32)) * (long)(Math.random() * (1l<<32))) &
+          ((1l << bitWidth) - 1l);
     }
     LOG.debug("Input:  " + TestBitPacking.toString(values));
     return values;

apache/parquet-java

parquet-column/src/test/java/org/apache/parquet/column/values/delta/DeltaBinaryPackingValuesWriterTest.java
@@ -45,7 +45,7 @@ public class DeltaBinaryPackingValuesWriterTest {
     blockSize = 128;
     miniBlockNum = 4;
     writer = new DeltaBinaryPackingValuesWriter(blockSize, miniBlockNum, 100, 200, new DirectByteBufferAllocator());
-    random = new Random();
+    random = new Random(0);
   }
 
   @Test(expected = IllegalArgumentException.class)
@@ -131,13 +131,26 @@ public class DeltaBinaryPackingValuesWriterTest {
   public void shouldReadMaxMinValue() throws IOException {
     int[] data = new int[10];
     for (int i = 0; i < data.length; i++) {
-      if(i%2==0) {
-        data[i]=Integer.MIN_VALUE;
-      }else {
-        data[i]=Integer.MAX_VALUE;
+      if (i % 2 == 0) {
+        data[i] = Integer.MIN_VALUE;
+      } else {
+        data[i] = Integer.MAX_VALUE;
       }
     }
     shouldWriteAndRead(data);
+  }
+
+  @Test
+  public void shouldReadMaxMinValueLong() throws IOException {
+    long[] data = new long[10];
+    for (int i = 0; i < data.length; i++) {
+      if (i % 2 == 0) {
+        data[i] = Long.MIN_VALUE;
+      } else {
+        data[i] = Long.MAX_VALUE;
+      }
+    }
+    shouldReadAndWriteLong(data, data.length);
   }
 
   @Test
@@ -211,6 +224,22 @@ public class DeltaBinaryPackingValuesWriterTest {
   }
 
   @Test
+  public void randomDataTestLong() throws IOException {
+    int maxSize = 1000;
+    long[] data = new long[maxSize];
+
+    for (int round = 0; round < 100000; round++) {
+      int size = random.nextInt(maxSize);
+
+      for (int i = 0; i < size; i++) {
+        data[i] = random.nextLong();
+      }
+      shouldReadAndWriteLong(data, size);
+      writer.reset();
+    }
+  }
+
+  @Test
   public void randomDataTest() throws IOException {
     int maxSize = 1000;
     int[] data = new int[maxSize];
@@ -230,6 +259,13 @@ public class DeltaBinaryPackingValuesWriterTest {
 
   private void shouldWriteAndRead(int[] data) throws IOException {
     shouldReadAndWrite(data, data.length);
+    writer.reset();
+    
+    long[] dataInLong = new long[data.length]; 
+    for (int i = 0; i < data.length; i++) {
+      dataInLong[i] = (long) data[i];
+    }
+    shouldReadAndWriteLong(dataInLong, data.length);
   }
 
   private void shouldReadAndWrite(int[] data, int length) throws IOException {
@@ -259,6 +295,36 @@ public class DeltaBinaryPackingValuesWriterTest {
   private void writeData(int[] data, int length) {
     for (int i = 0; i < length; i++) {
       writer.writeInteger(data[i]);
+    }
+  }
+
+  private void shouldReadAndWriteLong(long[] data, int length) throws IOException {
+    writeDataLong(data, length);
+    reader = new DeltaBinaryPackingValuesReader();
+    byte[] page = writer.getBytes().toByteArray();
+    int miniBlockSize = blockSize / miniBlockNum;
+
+    double miniBlockFlushed = Math.ceil(((double) length - 1) / miniBlockSize);
+    double blockFlushed = Math.ceil(((double) length - 1) / blockSize);
+    double estimatedSize = 3 * 5 + 1 * 10 //blockHeader, 3 * int + 1 * long
+        + 8 * miniBlockFlushed * miniBlockSize //data(aligned to miniBlock)
+        + blockFlushed * miniBlockNum //bitWidth of mini blocks
+        + (10.0 * blockFlushed);//min delta for each block
+    assertTrue(estimatedSize >= page.length);
+    reader.initFromPage(100, page, 0);
+
+    for (int i = 0; i < length; i++) {
+      assertEquals(data[i], reader.readLong());
+    }
+  }
+
+  private void writeDataLong(long[] data) {
+    writeDataLong(data, data.length);
+  }
+
+  private void writeDataLong(long[] data, int length) {
+    for (int i = 0; i < length; i++) {
+      writer.writeLong(data[i]);
     }
   }
 }

parquet-encoding/src/test/java/org/apache/parquet/column/values/bitpacking/TestByteBitPacking.java
@@ -46,6 +46,24 @@ public class TestByteBitPacking {
       Assert.assertArrayEquals("width "+i, values, unpacked);
     }
   }
+  
+  @Test
+  public void testPackUnPackLong() {
+    LOG.debug("");
+    LOG.debug("testPackUnPackLong");
+    for (int i = 1; i < 64; i++) {
+      LOG.debug("Width: " + i);
+      long[] unpacked32 = new long[32];
+      long[] unpacked8 = new long[32];
+      long[] values = generateValuesLong(i);
+      packUnpack32(Packer.BIG_ENDIAN.newBytePackerForLong(i), values, unpacked32);
+      LOG.debug("Output 32: " + TestBitPacking.toString(unpacked32));
+      Assert.assertArrayEquals("width "+i, values, unpacked32);
+      packUnpack8(Packer.BIG_ENDIAN.newBytePackerForLong(i), values, unpacked8);
+      LOG.debug("Output 8: " + TestBitPacking.toString(unpacked8));
+      Assert.assertArrayEquals("width "+i, values, unpacked8);
+    }
+  }
 
   private void packUnpack(BytePacker packer, int[] values, int[] unpacked) {
     byte[] packed = new byte[packer.getBitWidth() * 4];
@@ -54,10 +72,38 @@ public class TestByteBitPacking {
     packer.unpack32Values(ByteBuffer.wrap(packed), 0, unpacked, 0);
   }
 
+  private void packUnpack32(BytePackerForLong packer, long[] values, long[] unpacked) {
+    byte[] packed = new byte[packer.getBitWidth() * 4];
+    packer.pack32Values(values, 0, packed, 0);
+    LOG.debug("packed: " + TestBitPacking.toString(packed));
+    packer.unpack32Values(packed, 0, unpacked, 0);
+  }
+
+  private void packUnpack8(BytePackerForLong packer, long[] values, long[] unpacked) {
+    byte[] packed = new byte[packer.getBitWidth() * 4];
+    for (int i = 0; i < 4; i++) {
+      packer.pack8Values(values,  8 * i, packed, packer.getBitWidth() * i);
+    }
+    LOG.debug("packed: " + TestBitPacking.toString(packed));
+    for (int i = 0; i < 4; i++) {
+      packer.unpack8Values(packed, packer.getBitWidth() * i, unpacked, 8 * i);
+    }
+  }
+
   private int[] generateValues(int bitWidth) {
     int[] values = new int[32];
     for (int j = 0; j < values.length; j++) {
       values[j] = (int)(Math.random() * 100000) % (int)Math.pow(2, bitWidth);
+    }
+    LOG.debug("Input:  " + TestBitPacking.toString(values));
+    return values;
+  }
+
+  private long[] generateValuesLong(int bitWidth) {
+    long[] values = new long[32];
+    for (int j = 0; j < values.length; j++) {
+      values[j] = ((long)(Math.random() * (1l<<32)) * (long)(Math.random() * (1l<<32))) &
+          ((1l << bitWidth) - 1l);
     }
     LOG.debug("Input:  " + TestBitPacking.toString(values));
     return values;