113fec55c6966b506cd111f8b9d99b8199b3ba9c
Liwei Lin <proflin.me@gmail.com>
2016-02-03 11:47:42 +0800

    replace Log.java with slf4j for module [parquet-pig]

--- parquet-pig/src/main/java/org/apache/parquet/pig/ParquetLoader.java
+++ parquet-pig/src/main/java/org/apache/parquet/pig/ParquetLoader.java
@@ -20,7 +20,6 @@ package org.apache.parquet.pig;
 
 import static java.util.Arrays.asList;
 import static org.apache.hadoop.mapreduce.lib.input.FileInputFormat.setInputPaths;
-import static org.apache.parquet.Log.DEBUG;
 import static org.apache.parquet.hadoop.util.ContextUtil.getConfiguration;
 import static org.apache.parquet.pig.PigSchemaConverter.parsePigSchema;
 import static org.apache.parquet.pig.PigSchemaConverter.pigSchemaToString;
@@ -57,10 +56,12 @@ import org.apache.pig.impl.logicalLayer.schema.Schema.FieldSchema;
 import org.apache.pig.impl.util.UDFContext;
 import org.apache.pig.parser.ParserException;
 
-import org.apache.parquet.Log;
 import org.apache.parquet.hadoop.ParquetInputFormat;
 import org.apache.parquet.hadoop.metadata.GlobalMetaData;
 import org.apache.parquet.io.ParquetDecodingException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  *
@@ -71,7 +72,7 @@ import org.apache.parquet.io.ParquetDecodingException;
  *
  */
 public class ParquetLoader extends LoadFunc implements LoadMetadata, LoadPushDown {
-  private static final Log LOG = Log.getLog(ParquetLoader.class);
+  private static final Logger LOGGER = LoggerFactory.getLogger(ParquetLoader.class);
 
   // Using a weak hash map will ensure that the cache will be gc'ed when there is memory pressure
   static final Map<String, Reference<ParquetInputFormat<Tuple>>> inputFormatCache = new WeakHashMap<String, Reference<ParquetInputFormat<Tuple>>>();
@@ -137,7 +138,9 @@ public class ParquetLoader extends LoadFunc implements LoadMetadata, LoadPushDow
 
   @Override
   public void setLocation(String location, Job job) throws IOException {
-    if (DEBUG) LOG.debug("LoadFunc.setLocation(" + location + ", " + job + ")");
+    if (LOGGER.isDebugEnabled()) {
+      LOGGER.debug("LoadFunc.setLocation(" + location + ", " + job + ")");
+    }
 
     setInput(location, job);
   }
@@ -173,7 +176,9 @@ public class ParquetLoader extends LoadFunc implements LoadMetadata, LoadPushDow
 
   @Override
   public InputFormat<Void, Tuple> getInputFormat() throws IOException {
-    if (DEBUG) LOG.debug("LoadFunc.getInputFormat()");
+    if (LOGGER.isDebugEnabled()) {
+      LOGGER.debug("LoadFunc.getInputFormat()");
+    }
     return getParquetInputFormat();
   }
 
@@ -220,7 +225,9 @@ public class ParquetLoader extends LoadFunc implements LoadMetadata, LoadPushDow
   @Override
   public void prepareToRead(@SuppressWarnings("rawtypes") RecordReader reader, PigSplit split)
       throws IOException {
-    if (DEBUG) LOG.debug("LoadFunc.prepareToRead(" + reader + ", " + split + ")");
+    if (LOGGER.isDebugEnabled()) {
+      LOGGER.debug("LoadFunc.prepareToRead(" + reader + ", " + split + ")");
+    }
     this.reader = reader;
   }
 
@@ -240,14 +247,18 @@ public class ParquetLoader extends LoadFunc implements LoadMetadata, LoadPushDow
 
   @Override
   public String[] getPartitionKeys(String location, Job job) throws IOException {
-    if (DEBUG) LOG.debug("LoadMetadata.getPartitionKeys(" + location + ", " + job + ")");
+    if (LOGGER.isDebugEnabled()) {
+      LOGGER.debug("LoadMetadata.getPartitionKeys(" + location + ", " + job + ")");
+    }
     setInput(location, job);
     return null;
   }
 
   @Override
   public ResourceSchema getSchema(String location, Job job) throws IOException {
-    if (DEBUG) LOG.debug("LoadMetadata.getSchema(" + location + ", " + job + ")");
+    if (LOGGER.isDebugEnabled()) {
+      LOGGER.debug("LoadMetadata.getSchema(" + location + ", " + job + ")");
+    }
     setInput(location, job);
     return new ResourceSchema(schema);
   }
@@ -289,7 +300,9 @@ public class ParquetLoader extends LoadFunc implements LoadMetadata, LoadPushDow
   @Override
   public ResourceStatistics getStatistics(String location, Job job)
       throws IOException {
-    if (DEBUG) LOG.debug("LoadMetadata.getStatistics(" + location + ", " + job + ")");
+    if (LOGGER.isDebugEnabled()) {
+      LOGGER.debug("LoadMetadata.getStatistics(" + location + ", " + job + ")");
+    }
     /* We need to call setInput since setLocation is not
        guaranteed to be called before this */
     setInput(location, job);
@@ -299,7 +312,7 @@ public class ParquetLoader extends LoadFunc implements LoadMetadata, LoadPushDow
         length += split.getLength();
       }
     } catch (InterruptedException e) {
-      LOG.warn("Interrupted: ", e);
+      LOGGER.warn("Interrupted: ", e);
       return null;
     }
     ResourceStatistics stats = new ResourceStatistics();
@@ -310,7 +323,9 @@ public class ParquetLoader extends LoadFunc implements LoadMetadata, LoadPushDow
 
   @Override
   public void setPartitionFilter(Expression expression) throws IOException {
-    if (DEBUG) LOG.debug("LoadMetadata.setPartitionFilter(" + expression + ")");
+    if (LOGGER.isDebugEnabled()) {
+      LOGGER.debug("LoadMetadata.setPartitionFilter(" + expression + ")");
+    }
   }
 
   @Override
