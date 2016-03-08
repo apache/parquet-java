e1c7c22de8c7e7db61696e8ee6111b42c0f7ddfc
Liwei Lin <proflin.me@gmail.com>
2016-03-08 14:04:45 +0800

    Fix minor Log issue

--- parquet-pig/src/main/java/org/apache/parquet/pig/ParquetLoader.java
+++ parquet-pig/src/main/java/org/apache/parquet/pig/ParquetLoader.java
@@ -472,10 +472,10 @@ public class ParquetLoader extends LoadFunc implements LoadMetadata, LoadPushDow
 
   @Override
   public void setPushdownPredicate(Expression e) throws IOException {
-    LOG.info("Pig pushdown expression: " + e);
+    LOGGER.info("Pig pushdown expression: " + e);
 
     FilterPredicate pred = buildFilter(e);
-    LOG.info("Parquet filter predicate expression: " + pred);
+    LOGGER.info("Parquet filter predicate expression: " + pred);
 
     storeInUDFContext(ParquetInputFormat.FILTER_PREDICATE, pred);
   }
