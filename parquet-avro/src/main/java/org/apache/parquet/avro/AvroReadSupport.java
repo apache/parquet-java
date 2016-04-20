6b24a1d1b5e2792a7821ad172a45e38d2b04f9b8
Ryan Blue <blue@apache.org>
2016-04-20 08:41:22 -0700

    PARQUET-358: Add support for Avro's logical types API.
    This adds support for Avro's logical types API to parquet-avro.
    
    * The logical types API was introduced in Avro 1.8.0, so this bumps the Avro dependency version to 1.8.0.
    * Types supported are: decimal, date, time-millis, time-micros, timestamp-millis, and timestamp-micros
    * Tests have been copied from Avro and ported to the parquet-avro API
    
    Author: Ryan Blue <blue@apache.org>
    
    Closes #318 from rdblue/PARQUET-358-add-avro-logical-types-api and squashes the following commits:
    
    bd81f9c [Ryan Blue] PARQUET-358: Fix review items.
    0a882ee [Ryan Blue] PARQUET-358: Add logical types circular reference test.
    5124618 [Ryan Blue] PARQUET-358: Add license documentation for code from Avro.
    dcb14be [Ryan Blue] PARQUET-358: Add support for Avro's logical types API.

--- parquet-avro/src/main/java/org/apache/parquet/avro/AvroReadSupport.java
+++ parquet-avro/src/main/java/org/apache/parquet/avro/AvroReadSupport.java
@@ -110,9 +110,9 @@ public class AvroReadSupport<T> extends ReadSupport<T> {
     MessageType parquetSchema = readContext.getRequestedSchema();
     Schema avroSchema;
 
-    if (readContext.getReadSupportMetadata().get(AVRO_READ_SCHEMA_METADATA_KEY) != null) {
+    if (metadata.get(AVRO_READ_SCHEMA_METADATA_KEY) != null) {
       // use the Avro read schema provided by the user
-      avroSchema = new Schema.Parser().parse(readContext.getReadSupportMetadata().get(AVRO_READ_SCHEMA_METADATA_KEY));
+      avroSchema = new Schema.Parser().parse(metadata.get(AVRO_READ_SCHEMA_METADATA_KEY));
     } else if (keyValueMetaData.get(AVRO_SCHEMA_METADATA_KEY) != null) {
       // use the Avro schema from the file metadata if present
       avroSchema = new Schema.Parser().parse(keyValueMetaData.get(AVRO_SCHEMA_METADATA_KEY));
