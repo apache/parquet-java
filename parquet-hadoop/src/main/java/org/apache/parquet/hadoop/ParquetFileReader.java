41807ee8b4da989ba37b55e79a49d939e4162606
Piyush Narang <pnarang@twitter.com>
2016-05-24 14:46:34 -0700

    More logging

--- parquet-hadoop/src/main/java/org/apache/parquet/hadoop/ParquetFileReader.java
+++ parquet-hadoop/src/main/java/org/apache/parquet/hadoop/ParquetFileReader.java
@@ -1046,7 +1046,8 @@ public class ParquetFileReader implements Closeable {
 
       //Allocate the bytebuffer based on whether the FS can support it.
       ByteBuffer chunksByteBuffer = allocator.allocate(length);
-      CompatibilityUtil.getBuf(f, chunksByteBuffer);
+      int res = CompatibilityUtil.getBuf(f, chunksByteBuffer);
+      LOG.info("Requested to read: " + length + " ended up reading: " + res);
 
       // report in a counter the data we just scanned
       BenchmarkCounter.incrementBytesRead(length);
