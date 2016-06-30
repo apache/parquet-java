apache/parquet-java

commit 6b24a1d1b5e2792a7821ad172a45e38d2b04f9b8

PARQUET-358: Add support for Avro's logical types API.
This adds support for Avro's logical types API to parquet-avro.

* The logical types API was introduced in Avro 1.8.0, so this bumps the Avro dependency version to 1.8.0.
* Types supported are: decimal, date, time-millis, time-micros, timestamp-millis, and timestamp-micros
* Tests have been copied from Avro and ported to the parquet-avro API

Author: Ryan Blue <blue@apache.org>

Closes #318 from rdblue/PARQUET-358-add-avro-logical-types-api and squashes the following commits:

bd81f9c [Ryan Blue] PARQUET-358: Fix review items.
0a882ee [Ryan Blue] PARQUET-358: Add logical types circular reference test.
5124618 [Ryan Blue] PARQUET-358: Add license documentation for code from Avro.
dcb14be [Ryan Blue] PARQUET-358: Add support for Avro's logical types API.

parquet-avro/src/main/java/org/apache/parquet/avro/AvroWriteSupport.java
@@ -23,6 +23,8 @@ import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import org.apache.avro.Conversion;
+import org.apache.avro.LogicalType;
 import org.apache.avro.Schema;
 import org.apache.avro.generic.GenericData;
 import org.apache.avro.generic.GenericFixed;
@@ -69,6 +71,8 @@ public class AvroWriteSupport<T> extends WriteSupport<T> {
   private RecordConsumer recordConsumer;
   private MessageType rootSchema;
   private Schema rootAvroSchema;
+  private LogicalType rootLogicalType;
+  private Conversion<?> rootConversion;
   private GenericData model;
   private ListWriter listWriter;
 
@@ -82,6 +86,7 @@ public class AvroWriteSupport<T> extends WriteSupport<T> {
   public AvroWriteSupport(MessageType schema, Schema avroSchema) {
     this.rootSchema = schema;
     this.rootAvroSchema = avroSchema;
+    this.rootLogicalType = rootAvroSchema.getLogicalType();
     this.model = null;
   }
 
@@ -89,6 +94,7 @@ public class AvroWriteSupport<T> extends WriteSupport<T> {
                           GenericData model) {
     this.rootSchema = schema;
     this.rootAvroSchema = avroSchema;
+    this.rootLogicalType = rootAvroSchema.getLogicalType();
     this.model = model;
   }
 
@@ -136,16 +142,25 @@ public class AvroWriteSupport<T> extends WriteSupport<T> {
   // overloaded version for backward compatibility
   @SuppressWarnings("unchecked")
   public void write(IndexedRecord record) {
-    recordConsumer.startMessage();
-    writeRecordFields(rootSchema, rootAvroSchema, record);
-    recordConsumer.endMessage();
+    write((T) record);
   }
 
   @Override
   public void write(T record) {
-    recordConsumer.startMessage();
-    writeRecordFields(rootSchema, rootAvroSchema, record);
-    recordConsumer.endMessage();
+    if (rootLogicalType != null) {
+      Conversion<?> conversion = model.getConversionByClass(
+          record.getClass(), rootLogicalType);
+
+      recordConsumer.startMessage();
+      writeRecordFields(rootSchema, rootAvroSchema,
+          convert(rootAvroSchema, rootLogicalType, conversion, record));
+      recordConsumer.endMessage();
+
+    } else {
+      recordConsumer.startMessage();
+      writeRecordFields(rootSchema, rootAvroSchema, record);
+      recordConsumer.endMessage();
+    }
   }
 
   private void writeRecord(GroupType schema, Schema avroSchema,
@@ -226,6 +241,8 @@ public class AvroWriteSupport<T> extends WriteSupport<T> {
       }
     }
 
+    // TODO: what if the value is null?
+
     // Sparsely populated method of encoding unions, each member has its own
     // set of columns.
     String memberName = "member" + parquetIndex;
@@ -237,44 +254,108 @@ public class AvroWriteSupport<T> extends WriteSupport<T> {
     recordConsumer.endGroup();
   }
 
-  @SuppressWarnings("unchecked")
+  /**
+   * Calls an appropriate write method based on the value.
+   * Value MUST not be null.
+   *
+   * @param type the Parquet type
+   * @param avroSchema the Avro schema
+   * @param value a non-null value to write
+   */
   private void writeValue(Type type, Schema avroSchema, Object value) {
     Schema nonNullAvroSchema = AvroSchemaConverter.getNonNull(avroSchema);
-    Schema.Type avroType = nonNullAvroSchema.getType();
-    if (avroType.equals(Schema.Type.BOOLEAN)) {
-      recordConsumer.addBoolean((Boolean) value);
-    } else if (avroType.equals(Schema.Type.INT)) {
-      if (value instanceof Character) {
-        recordConsumer.addInteger((Character) value);
-      } else {
-        recordConsumer.addInteger(((Number) value).intValue());
-      }
-    } else if (avroType.equals(Schema.Type.LONG)) {
-      recordConsumer.addLong(((Number) value).longValue());
-    } else if (avroType.equals(Schema.Type.FLOAT)) {
-      recordConsumer.addFloat(((Number) value).floatValue());
-    } else if (avroType.equals(Schema.Type.DOUBLE)) {
-      recordConsumer.addDouble(((Number) value).doubleValue());
-    } else if (avroType.equals(Schema.Type.BYTES)) {
-      if (value instanceof byte[]) {
-        recordConsumer.addBinary(Binary.fromReusedByteArray((byte[]) value));
-      } else {
-        recordConsumer.addBinary(Binary.fromReusedByteBuffer((ByteBuffer) value));
-      }
-    } else if (avroType.equals(Schema.Type.STRING)) {
-      recordConsumer.addBinary(fromAvroString(value));
-    } else if (avroType.equals(Schema.Type.RECORD)) {
-      writeRecord(type.asGroupType(), nonNullAvroSchema, value);
-    } else if (avroType.equals(Schema.Type.ENUM)) {
-      recordConsumer.addBinary(Binary.fromString(value.toString()));
-    } else if (avroType.equals(Schema.Type.ARRAY)) {
-      listWriter.writeList(type.asGroupType(), nonNullAvroSchema, value);
-    } else if (avroType.equals(Schema.Type.MAP)) {
-      writeMap(type.asGroupType(), nonNullAvroSchema, (Map<CharSequence, ?>) value);
-    } else if (avroType.equals(Schema.Type.UNION)) {
-      writeUnion(type.asGroupType(), nonNullAvroSchema, value);
-    } else if (avroType.equals(Schema.Type.FIXED)) {
-      recordConsumer.addBinary(Binary.fromReusedByteArray(((GenericFixed) value).bytes()));
+    LogicalType logicalType = nonNullAvroSchema.getLogicalType();
+    if (logicalType != null) {
+      Conversion<?> conversion = model.getConversionByClass(
+          value.getClass(), logicalType);
+      writeValueWithoutConversion(type, nonNullAvroSchema,
+          convert(nonNullAvroSchema, logicalType, conversion, value));
+    } else {
+      writeValueWithoutConversion(type, nonNullAvroSchema, value);
+    }
+  }
+
+  private <D> Object convert(Schema schema, LogicalType logicalType,
+                             Conversion<D> conversion, Object datum) {
+    if (conversion == null) {
+      return datum;
+    }
+    Class<D> fromClass = conversion.getConvertedType();
+    switch (schema.getType()) {
+      case RECORD:  return conversion.toRecord(fromClass.cast(datum), schema, logicalType);
+      case ENUM:    return conversion.toEnumSymbol(fromClass.cast(datum), schema, logicalType);
+      case ARRAY:   return conversion.toArray(fromClass.cast(datum), schema, logicalType);
+      case MAP:     return conversion.toMap(fromClass.cast(datum), schema, logicalType);
+      case FIXED:   return conversion.toFixed(fromClass.cast(datum), schema, logicalType);
+      case STRING:  return conversion.toCharSequence(fromClass.cast(datum), schema, logicalType);
+      case BYTES:   return conversion.toBytes(fromClass.cast(datum), schema, logicalType);
+      case INT:     return conversion.toInt(fromClass.cast(datum), schema, logicalType);
+      case LONG:    return conversion.toLong(fromClass.cast(datum), schema, logicalType);
+      case FLOAT:   return conversion.toFloat(fromClass.cast(datum), schema, logicalType);
+      case DOUBLE:  return conversion.toDouble(fromClass.cast(datum), schema, logicalType);
+      case BOOLEAN: return conversion.toBoolean(fromClass.cast(datum), schema, logicalType);
+    }
+    return datum;
+  }
+
+  /**
+   * Calls an appropriate write method based on the value.
+   * Value must not be null and the schema must not be nullable.
+   *
+   * @param type a Parquet type
+   * @param avroSchema a non-nullable Avro schema
+   * @param value a non-null value to write
+   */
+  @SuppressWarnings("unchecked")
+  private void writeValueWithoutConversion(Type type, Schema avroSchema, Object value) {
+    switch (avroSchema.getType()) {
+      case BOOLEAN:
+        recordConsumer.addBoolean((Boolean) value);
+        break;
+      case INT:
+        if (value instanceof Character) {
+          recordConsumer.addInteger((Character) value);
+        } else {
+          recordConsumer.addInteger(((Number) value).intValue());
+        }
+        break;
+      case LONG:
+        recordConsumer.addLong(((Number) value).longValue());
+        break;
+      case FLOAT:
+        recordConsumer.addFloat(((Number) value).floatValue());
+        break;
+      case DOUBLE:
+        recordConsumer.addDouble(((Number) value).doubleValue());
+        break;
+      case FIXED:
+        recordConsumer.addBinary(Binary.fromReusedByteArray(((GenericFixed) value).bytes()));
+        break;
+      case BYTES:
+        if (value instanceof byte[]) {
+          recordConsumer.addBinary(Binary.fromReusedByteArray((byte[]) value));
+        } else {
+          recordConsumer.addBinary(Binary.fromReusedByteBuffer((ByteBuffer) value));
+        }
+        break;
+      case STRING:
+        recordConsumer.addBinary(fromAvroString(value));
+        break;
+      case RECORD:
+        writeRecord(type.asGroupType(), avroSchema, value);
+        break;
+      case ENUM:
+        recordConsumer.addBinary(Binary.fromString(value.toString()));
+        break;
+      case ARRAY:
+        listWriter.writeList(type.asGroupType(), avroSchema, value);
+        break;
+      case MAP:
+        writeMap(type.asGroupType(), avroSchema, (Map<CharSequence, ?>) value);
+        break;
+      case UNION:
+        writeUnion(type.asGroupType(), avroSchema, value);
+        break;
     }
   }
 
@@ -283,7 +364,7 @@ public class AvroWriteSupport<T> extends WriteSupport<T> {
       Utf8 utf8 = (Utf8) value;
       return Binary.fromReusedByteArray(utf8.getBytes(), 0, utf8.getByteLength());
     }
-    return Binary.fromString(value.toString());
+    return Binary.fromString((CharSequence) value);
   }
 
   private static GenericData getDataModel(Configuration conf) {

parquet-column/src/main/java/org/apache/parquet/io/api/Binary.java
@@ -25,6 +25,10 @@ import java.io.OutputStream;
 import java.io.Serializable;
 import java.io.UnsupportedEncodingException;
 import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
+import java.nio.charset.CharacterCodingException;
+import java.nio.charset.CharsetEncoder;
+import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 
 import org.apache.parquet.io.ParquetEncodingException;
@@ -209,25 +213,32 @@ abstract public class Binary implements Comparable<Binary>, Serializable {
 
   }
 
-  private static class FromStringBinary extends ByteArrayBackedBinary {
-    public FromStringBinary(String value) {
-      // reused is false, because we do not
-      // hold on to the underlying bytes,
-      // and nobody else has a handle to them
+  private static class FromStringBinary extends ByteBufferBackedBinary {
+    public FromStringBinary(CharSequence value) {
+      // reused is false, because we do not hold on to the buffer after
+      // conversion, and nobody else has a handle to it
       super(encodeUTF8(value), false);
-    }
-
-    private static byte[] encodeUTF8(String value) {
-      try {
-        return value.getBytes("UTF-8");
-      } catch (UnsupportedEncodingException e) {
-        throw new ParquetEncodingException("UTF-8 not supported.", e);
-      }
     }
 
     @Override
     public String toString() {
       return "Binary{\"" + toStringUsingUTF8() + "\"}";
+    }
+
+    private static final ThreadLocal<CharsetEncoder> ENCODER =
+        new ThreadLocal<CharsetEncoder>() {
+          @Override
+          protected CharsetEncoder initialValue() {
+            return StandardCharsets.UTF_8.newEncoder();
+          }
+        };
+
+    private static ByteBuffer encodeUTF8(CharSequence value) {
+      try {
+        return ENCODER.get().encode(CharBuffer.wrap(value));
+      } catch (CharacterCodingException e) {
+        throw new ParquetEncodingException("UTF-8 not supported.", e);
+      }
     }
   }
 
@@ -358,6 +369,13 @@ abstract public class Binary implements Comparable<Binary>, Serializable {
     private transient byte[] cachedBytes;
     private int offset;
     private int length;
+
+    public ByteBufferBackedBinary(ByteBuffer value, boolean isBackingBytesReused) {
+      this.value = value;
+      this.offset = value.position();
+      this.length = value.remaining();
+      this.isBackingBytesReused = isBackingBytesReused;
+    }
 
     public ByteBufferBackedBinary(ByteBuffer value, int offset, int length, boolean isBackingBytesReused) {
       this.value = value;
@@ -521,11 +539,11 @@ abstract public class Binary implements Comparable<Binary>, Serializable {
   }
 
   public static Binary fromReusedByteBuffer(final ByteBuffer value) {
-    return new ByteBufferBackedBinary(value, value.position(), value.remaining(), true);
+    return new ByteBufferBackedBinary(value, true);
   }
 
   public static Binary fromConstantByteBuffer(final ByteBuffer value) {
-    return new ByteBufferBackedBinary(value, value.position(), value.remaining(), false);
+    return new ByteBufferBackedBinary(value, false);
   }
 
   @Deprecated
@@ -536,7 +554,12 @@ abstract public class Binary implements Comparable<Binary>, Serializable {
     return fromReusedByteBuffer(value); // Assume producer intends to reuse byte[]
   }
 
-  public static Binary fromString(final String value) {
+  public static Binary fromString(String value) {
+    // this method is for binary backward-compatibility
+    return fromString((CharSequence) value);
+  }
+
+  public static Binary fromString(CharSequence value) {
     return new FromStringBinary(value);
   }
 

NOTICE
@@ -43,3 +43,14 @@ with the following copyright notice:
   See the License for the specific language governing permissions and
   limitations under the License.
 
+--------------------------------------------------------------------------------
+
+This product includes code from Apache Avro, which includes the following in
+its NOTICE file:
+
+  Apache Avro
+  Copyright 2010-2015 The Apache Software Foundation
+
+  This product includes software developed at
+  The Apache Software Foundation (http://www.apache.org/).
+

parquet-avro/pom.xml
@@ -32,10 +32,6 @@
   <name>Apache Parquet Avro</name>
   <url>https://parquet.apache.org</url>
 
-  <properties>
-    <avro.version>1.7.6</avro.version>
-  </properties>
-
   <dependencies>
     <dependency>
       <groupId>org.apache.parquet</groupId>

parquet-avro/src/main/java/org/apache/parquet/avro/AvroIndexedRecordConverter.java
@@ -21,6 +21,8 @@ package org.apache.parquet.avro;
 import java.lang.reflect.Constructor;
 import java.util.HashMap;
 import java.util.Map;
+import org.apache.avro.Conversion;
+import org.apache.avro.LogicalType;
 import org.apache.avro.Schema;
 import org.apache.avro.generic.GenericArray;
 import org.apache.avro.generic.GenericData;
@@ -111,6 +113,11 @@ class AvroIndexedRecordConverter<T extends IndexedRecord> extends GroupConverter
 
   @SuppressWarnings("unchecked")
   private static <T> Class<T> getDatumClass(GenericData model, Schema schema) {
+    if (model.getConversionFor(schema.getLogicalType()) != null) {
+      // use generic classes to pass data to conversions
+      return null;
+    }
+
     if (model instanceof SpecificData) {
       return (Class<T>) ((SpecificData) model).getClass(schema);
     }
@@ -133,7 +140,16 @@ class AvroIndexedRecordConverter<T extends IndexedRecord> extends GroupConverter
   }
 
   private static Converter newConverter(Schema schema, Type type,
-      GenericData model, ParentValueContainer parent) {
+      GenericData model, ParentValueContainer setter) {
+
+    LogicalType logicalType = schema.getLogicalType();
+    // the expected type is always null because it is determined by the parent
+    // datum class, which never helps for generic. when logical types are added
+    // to specific, this should pass the expected type here.
+    Conversion<?> conversion = model.getConversionFor(logicalType);
+    ParentValueContainer parent = ParentValueContainer
+        .getConversionContainer(setter, conversion, schema);
+
     if (schema.getType().equals(Schema.Type.BOOLEAN)) {
       return new AvroConverters.FieldBooleanConverter(parent);
     } else if (schema.getType().equals(Schema.Type.INT)) {

parquet-avro/src/main/java/org/apache/parquet/avro/AvroReadSupport.java
@@ -110,9 +110,9 @@ public class AvroReadSupport<T> extends ReadSupport<T> {
     MessageType parquetSchema = readContext.getRequestedSchema();
     Schema avroSchema;
 
-    if (readContext.getReadSupportMetadata().get(AVRO_READ_SCHEMA_METADATA_KEY) != null) {
+    if (metadata.get(AVRO_READ_SCHEMA_METADATA_KEY) != null) {
       // use the Avro read schema provided by the user
-      avroSchema = new Schema.Parser().parse(readContext.getReadSupportMetadata().get(AVRO_READ_SCHEMA_METADATA_KEY));
+      avroSchema = new Schema.Parser().parse(metadata.get(AVRO_READ_SCHEMA_METADATA_KEY));
     } else if (keyValueMetaData.get(AVRO_SCHEMA_METADATA_KEY) != null) {
       // use the Avro schema from the file metadata if present
       avroSchema = new Schema.Parser().parse(keyValueMetaData.get(AVRO_SCHEMA_METADATA_KEY));

commit 7f8e952abc4d2fc4b96c97a51aa25fcf6ed8af02

PARQUET-642: Improve performance of ByteBuffer based read / write paths
While trying out the newest Parquet version, we noticed that the changes to start using ByteBuffers: https://github.com/apache/parquet-mr/commit/6b605a4ea05b66e1a6bf843353abcb4834a4ced8 and https://github.com/apache/parquet-mr/commit/6b24a1d1b5e2792a7821ad172a45e38d2b04f9b8 (mostly avro but a couple of ByteBuffer changes) caused our jobs to slow down a bit.

Read overhead: 4-6% (in MB_Millis)
Write overhead: 6-10% (MB_Millis).

Seems like this seems to be due to the encoding / decoding of Strings in the [Binary class](https://github.com/apache/parquet-mr/blob/master/parquet-column/src/main/java/org/apache/parquet/io/api/Binary.java):
[toStringUsingUTF8()](https://github.com/apache/parquet-mr/blob/master/parquet-column/src/main/java/org/apache/parquet/io/api/Binary.java#L388) - for reads
[encodeUTF8()](https://github.com/apache/parquet-mr/blob/master/parquet-column/src/main/java/org/apache/parquet/io/api/Binary.java#L236) - for writes

With these changes we see around 5% improvement in MB_Millis while running the job on our Hadoop cluster.

Added some microbenchmark details to the jira.

Note that I've left the behavior the same for the avro write path - it still uses CharSequence and the Charset based encoders.

Author: Piyush Narang <pnarang@twitter.com>

Closes #347 from piyushnarang/bytebuffer-encoding-fix-pr and squashes the following commits:

43c5bdd [Piyush Narang] Keep avro on char sequence
2d50c8c [Piyush Narang] Update Binary approach
9e58237 [Piyush Narang] Proof of concept fixes

parquet-avro/src/main/java/org/apache/parquet/avro/AvroWriteSupport.java
@@ -364,7 +364,7 @@ public class AvroWriteSupport<T> extends WriteSupport<T> {
       Utf8 utf8 = (Utf8) value;
       return Binary.fromReusedByteArray(utf8.getBytes(), 0, utf8.getByteLength());
     }
-    return Binary.fromString((CharSequence) value);
+    return Binary.fromCharSequence((CharSequence) value);
   }
 
   private static GenericData getDataModel(Configuration conf) {

parquet-column/src/main/java/org/apache/parquet/io/api/Binary.java
@@ -31,6 +31,7 @@ import java.nio.charset.CharsetEncoder;
 import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 
+import org.apache.parquet.io.ParquetDecodingException;
 import org.apache.parquet.io.ParquetEncodingException;
 
 import static org.apache.parquet.bytes.BytesUtils.UTF8;
@@ -214,7 +215,28 @@ abstract public class Binary implements Comparable<Binary>, Serializable {
   }
 
   private static class FromStringBinary extends ByteBufferBackedBinary {
-    public FromStringBinary(CharSequence value) {
+    public FromStringBinary(String value) {
+      // reused is false, because we do not hold on to the buffer after
+      // conversion, and nobody else has a handle to it
+      super(encodeUTF8(value), false);
+    }
+
+    @Override
+    public String toString() {
+      return "Binary{\"" + toStringUsingUTF8() + "\"}";
+    }
+
+    private static ByteBuffer encodeUTF8(String value) {
+      try {
+        return ByteBuffer.wrap(value.getBytes("UTF-8"));
+      } catch (UnsupportedEncodingException e) {
+        throw new ParquetEncodingException("UTF-8 not supported.", e);
+      }
+    }
+  }
+
+  private static class FromCharSequenceBinary extends ByteBufferBackedBinary {
+    public FromCharSequenceBinary(CharSequence value) {
       // reused is false, because we do not hold on to the buffer after
       // conversion, and nobody else has a handle to it
       super(encodeUTF8(value), false);
@@ -226,12 +248,12 @@ abstract public class Binary implements Comparable<Binary>, Serializable {
     }
 
     private static final ThreadLocal<CharsetEncoder> ENCODER =
-        new ThreadLocal<CharsetEncoder>() {
-          @Override
-          protected CharsetEncoder initialValue() {
-            return StandardCharsets.UTF_8.newEncoder();
-          }
-        };
+      new ThreadLocal<CharsetEncoder>() {
+        @Override
+        protected CharsetEncoder initialValue() {
+          return StandardCharsets.UTF_8.newEncoder();
+        }
+      };
 
     private static ByteBuffer encodeUTF8(CharSequence value) {
       try {
@@ -386,16 +408,26 @@ abstract public class Binary implements Comparable<Binary>, Serializable {
 
     @Override
     public String toStringUsingUTF8() {
-      int limit = value.limit();
-      value.limit(offset+length);
-      int position = value.position();
-      value.position(offset);
-      // no corresponding interface to read a subset of a buffer, would have to slice it
-      // which creates another ByteBuffer object or do what is done here to adjust the
-      // limit/offset and set them back after
-      String ret = UTF8.decode(value).toString();
-      value.limit(limit);
-      value.position(position);
+      String ret;
+      if (value.hasArray()) {
+        try {
+          ret = new String(value.array(), value.arrayOffset() + offset, length, "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+          throw new ParquetDecodingException("UTF-8 not supported");
+        }
+      } else {
+        int limit = value.limit();
+        value.limit(offset+length);
+        int position = value.position();
+        value.position(offset);
+        // no corresponding interface to read a subset of a buffer, would have to slice it
+        // which creates another ByteBuffer object or do what is done here to adjust the
+        // limit/offset and set them back after
+        ret = UTF8.decode(value).toString();
+        value.limit(limit);
+        value.position(position);
+      }
+
       return ret;
     }
 
@@ -555,12 +587,11 @@ abstract public class Binary implements Comparable<Binary>, Serializable {
   }
 
   public static Binary fromString(String value) {
-    // this method is for binary backward-compatibility
-    return fromString((CharSequence) value);
+    return new FromStringBinary(value);
   }
 
-  public static Binary fromString(CharSequence value) {
-    return new FromStringBinary(value);
+  public static Binary fromCharSequence(CharSequence value) {
+    return new FromCharSequenceBinary(value);
   }
 
   /**