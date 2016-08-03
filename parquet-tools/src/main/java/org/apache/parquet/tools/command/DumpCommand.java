ea402becca436dc1a8e47ac9385a3db475b49355
djhworld <djharperuk@gmail.com>
2016-08-03 14:14:26 -0700

    PARQUET-668 - Provide option to disable auto crop feature in dump
    https://issues.apache.org/jira/browse/PARQUET-668
    
    1. Added option `--disable-crop`
    2. Updated `README.md` to reflect changes
    
    Author: djhworld <djharperuk@gmail.com>
    
    Closes #358 from djhworld/master and squashes the following commits:
    
    493c3d0 [djhworld] PARQUET-668: Removed usage instructions from README, replaced with --help flag
    696a5e6 [djhworld] PARQUET-668 -> Updated README.md to fix issue in usage string
    6cbf59b [djhworld] PARQUET-668 - Provide option to disable auto crop feature in DumpCommand output

--- parquet-tools/src/main/java/org/apache/parquet/tools/command/DumpCommand.java
+++ parquet-tools/src/main/java/org/apache/parquet/tools/command/DumpCommand.java
@@ -85,6 +85,10 @@ public class DumpCommand extends ArgsOnlyCommand {
                                  .withDescription("Do not dump column data")
                                  .create('d');
 
+        Option nocrop = OptionBuilder.withLongOpt("disable-crop")
+                                 .withDescription("Do not crop the output based on console width")
+                                 .create('n');
+
         Option cl = OptionBuilder.withLongOpt("column")
                                  .withDescription("Dump only the given column, can be specified more than once")
                                  .hasArgs()
@@ -92,6 +96,7 @@ public class DumpCommand extends ArgsOnlyCommand {
 
         OPTIONS.addOption(md);
         OPTIONS.addOption(dt);
+        OPTIONS.addOption(nocrop);
         OPTIONS.addOption(cl);
     }
 
@@ -122,17 +127,9 @@ public class DumpCommand extends ArgsOnlyCommand {
         ParquetMetadata metaData = ParquetFileReader.readFooter(conf, inpath, NO_FILTER);
         MessageType schema = metaData.getFileMetaData().getSchema();
 
-        PrettyPrintWriter out = PrettyPrintWriter.stdoutPrettyPrinter()
-                                                 .withAutoColumn()
-                                                 .withAutoCrop()
-                                                 .withWhitespaceHandler(WhiteSpaceHandler.ELIMINATE_NEWLINES)
-                                                 .withColumnPadding(1)
-                                                 .withMaxBufferedLines(1000000)
-                                                 .withFlushOnTab()
-                                                 .build();
-
         boolean showmd = !options.hasOption('m');
         boolean showdt = !options.hasOption('d');
+        boolean cropoutput = !options.hasOption('n');
 
         Set<String> showColumns = null;
         if (options.hasOption('c')) {
@@ -140,6 +137,7 @@ public class DumpCommand extends ArgsOnlyCommand {
             showColumns = new HashSet<String>(Arrays.asList(cols));
         }
 
+        PrettyPrintWriter out = prettyPrintWriter(cropoutput);
         dump(out, metaData, schema, inpath, showmd, showdt, showColumns);
     }
 
@@ -344,6 +342,21 @@ public class DumpCommand extends ArgsOnlyCommand {
         if (data == null) return null;
 
         return new BigInteger(data);
+    }
+
+    private static PrettyPrintWriter prettyPrintWriter(boolean cropOutput) {
+        PrettyPrintWriter.Builder builder = PrettyPrintWriter.stdoutPrettyPrinter()
+                .withAutoColumn()
+                .withWhitespaceHandler(WhiteSpaceHandler.ELIMINATE_NEWLINES)
+                .withColumnPadding(1)
+                .withMaxBufferedLines(1000000)
+                .withFlushOnTab();
+
+        if (cropOutput) {
+            builder.withAutoCrop();
+        }
+
+        return builder.build();
     }
 
     private static final class DumpGroupConverter extends GroupConverter {
