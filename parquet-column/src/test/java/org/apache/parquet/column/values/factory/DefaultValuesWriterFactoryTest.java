30aa91012cf6019bb9720609c1d03b5386a87ffb
Piyush Narang <pnarang@twitter.com>
2016-08-11 13:30:43 -0700

    PARQUET-601: Add support to configure the encoding used by ValueWriters
    ### Context:
    Parquet is currently structured to choose the appropriate value writer based on the type of the column as well as the Parquet version. As of now, the writer(s) (and hence encoding) for each data type is hard coded in the Parquet source code.
    
    This PR adds support for being able to override the encodings per type via config. That allows users to experiment with various encoding strategies manually as well as enables them to override the hardcoded defaults if they don't suit their use case.
    
    We can override encodings per data type (int32 / int64 / ...).
    Something on the lines of:
    ```
    parquet.writer.encoding-override.<type> = "encoding1[,encoding2]"
    ```
    
    As an example:
    ```
    "parquet.writer.encoding-override.int32" = "plain"
    (Chooses Plain encoding and hence the PlainValuesWriter).
    ```
    
    When a primary + fallback need to be specified, we can do the following:
    ```
    "parquet.writer.encoding-override.binary" = "rle_dictionary,delta_byte_array"
    (Chooses RLE_DICTIONARY encoding as the initial encoding and DELTA_BYTE_ARRAY encoding as the fallback and hence creates a FallbackWriter(PlainBinaryDictionaryValuesWriter, DeltaByteArrayWriter).
    ```
    
    In such cases we can mandate that the first encoding listed must allow for Fallbacks by implementing [RequiresFallback](https://github.com/apache/parquet-mr/blob/master/parquet-column/src/main/java/org/apache/parquet/column/values/RequiresFallback.java#L31).
    
    ### PR notes:
    
    - Restructured the ValuesWriter creation code. Pulled it out of ParquetProperties into a new class and refactored the flow based on type as it was getting hard to follow and I felt adding the overrides would make it harder. Added a bunch of unit tests to verify the ValuesWriter we create for combinations of type, parquet version and dictionary on / off.
    - Added unit tests to verify parsing of the encoding overrides + creation of ValuesWriters based on these overrides.
    - Manually tested some encoding overrides scenarios out on Hadoop (both parquet v1, v2).
    
    Author: Piyush Narang <pnarang@twitter.com>
    
    Closes #342 from piyushnarang/master and squashes the following commits:
    
    3ebab28 [Piyush Narang] Remove Configurable
    149bb98 [Piyush Narang] Switch to getValuesWriterFactory call to non-static
    0b78e04 [Piyush Narang] Address Ryan's feedback
    1da6ca3 [Piyush Narang] Merge branch 'master' into piyush/dynamic-encoding-overrides
    f021ed2 [Piyush Narang] Tweak comment in ValuesWriterFactory
    cb02ea0 [Piyush Narang] Fix review comments
    bf4bc6d [Piyush Narang] Add support for Config setting in ValuesWriter factory
    8a852a3 [Piyush Narang] Log values writer factory chosen
    e4b61a4 [Piyush Narang] Tweak factory instantiation a bit
    b46cccd [Piyush Narang] Add class based factory override
    6a5428f [Piyush Narang] Clean up some stuff in ValuesWriterFactory
    0f8cd09 [Piyush Narang] Refactor mockito version
    9ead61d [Piyush Narang] Add guava test dep
    5c636c7 [Piyush Narang] Add encoding-overrides config to ParquetOutputFormat config
    b9d6c13 [Piyush Narang] Refactor code in ValuesWriterFactory a bit
    ff4c90d [Piyush Narang] Pull out value writer creation to ValuesWriterFactory and add unit tests

---
+++ parquet-column/src/test/java/org/apache/parquet/column/values/factory/DefaultValuesWriterFactoryTest.java
@@ -0,0 +1,350 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.parquet.column.values.factory;
+
+import org.apache.parquet.column.ColumnDescriptor;
+import org.apache.parquet.column.ParquetProperties;
+import org.apache.parquet.column.ParquetProperties.WriterVersion;
+import org.apache.parquet.column.values.ValuesWriter;
+import org.apache.parquet.column.values.delta.DeltaBinaryPackingValuesWriter;
+import org.apache.parquet.column.values.delta.DeltaBinaryPackingValuesWriterForLong;
+import org.apache.parquet.column.values.deltastrings.DeltaByteArrayWriter;
+import org.apache.parquet.column.values.dictionary.DictionaryValuesWriter;
+import org.apache.parquet.column.values.dictionary.DictionaryValuesWriter.*;
+import org.apache.parquet.column.values.fallback.FallbackValuesWriter;
+import org.apache.parquet.column.values.plain.BooleanPlainValuesWriter;
+import org.apache.parquet.column.values.plain.FixedLenByteArrayPlainValuesWriter;
+import org.apache.parquet.column.values.plain.PlainValuesWriter;
+import org.apache.parquet.column.values.rle.RunLengthBitPackingHybridValuesWriter;
+import org.apache.parquet.schema.PrimitiveType.PrimitiveTypeName;
+
+import org.junit.Test;
+
+import static junit.framework.Assert.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class DefaultValuesWriterFactoryTest {
+
+  @Test
+  public void testBoolean() {
+    doTestValueWriter(
+      PrimitiveTypeName.BOOLEAN,
+      WriterVersion.PARQUET_1_0,
+      true,
+      BooleanPlainValuesWriter.class);
+  }
+
+  @Test
+  public void testBoolean_V2() {
+    doTestValueWriter(
+      PrimitiveTypeName.BOOLEAN,
+      WriterVersion.PARQUET_2_0,
+      true,
+      RunLengthBitPackingHybridValuesWriter.class);
+  }
+
+  @Test
+  public void testFixedLenByteArray() {
+    doTestValueWriter(
+      PrimitiveTypeName.FIXED_LEN_BYTE_ARRAY,
+      WriterVersion.PARQUET_1_0,
+      true,
+      FixedLenByteArrayPlainValuesWriter.class);
+  }
+
+  @Test
+  public void testFixedLenByteArray_V2() {
+    doTestValueWriter(
+      PrimitiveTypeName.FIXED_LEN_BYTE_ARRAY,
+      WriterVersion.PARQUET_2_0,
+      true,
+      DictionaryValuesWriter.class, DeltaByteArrayWriter.class);
+  }
+
+  @Test
+  public void testFixedLenByteArray_V2_NoDict() {
+    doTestValueWriter(
+      PrimitiveTypeName.FIXED_LEN_BYTE_ARRAY,
+      WriterVersion.PARQUET_2_0,
+      false,
+      DeltaByteArrayWriter.class);
+  }
+
+  @Test
+  public void testBinary() {
+    doTestValueWriter(
+      PrimitiveTypeName.BINARY,
+      WriterVersion.PARQUET_1_0,
+      true,
+      PlainBinaryDictionaryValuesWriter.class, PlainValuesWriter.class);
+  }
+
+  @Test
+  public void testBinary_NoDict() {
+    doTestValueWriter(
+      PrimitiveTypeName.BINARY,
+      WriterVersion.PARQUET_1_0,
+      false,
+      PlainValuesWriter.class);
+  }
+
+  @Test
+  public void testBinary_V2() {
+    doTestValueWriter(
+      PrimitiveTypeName.BINARY,
+      WriterVersion.PARQUET_2_0,
+      true,
+      PlainBinaryDictionaryValuesWriter.class, DeltaByteArrayWriter.class);
+  }
+
+  @Test
+  public void testBinary_V2_NoDict() {
+    doTestValueWriter(
+      PrimitiveTypeName.BINARY,
+      WriterVersion.PARQUET_2_0,
+      false,
+      DeltaByteArrayWriter.class);
+  }
+
+  @Test
+  public void testInt32() {
+    doTestValueWriter(
+      PrimitiveTypeName.INT32,
+      WriterVersion.PARQUET_1_0,
+      true,
+      PlainIntegerDictionaryValuesWriter.class, PlainValuesWriter.class);
+  }
+
+  @Test
+  public void testInt32_NoDict() {
+    doTestValueWriter(
+      PrimitiveTypeName.INT32,
+      WriterVersion.PARQUET_1_0,
+      false,
+      PlainValuesWriter.class);
+  }
+
+  @Test
+  public void testInt32_V2() {
+    doTestValueWriter(
+      PrimitiveTypeName.INT32,
+      WriterVersion.PARQUET_2_0,
+      true,
+      PlainIntegerDictionaryValuesWriter.class, DeltaBinaryPackingValuesWriter.class);
+  }
+
+  @Test
+  public void testInt32_V2_NoDict() {
+    doTestValueWriter(
+      PrimitiveTypeName.INT32,
+      WriterVersion.PARQUET_2_0,
+      false,
+      DeltaBinaryPackingValuesWriter.class);
+  }
+
+  @Test
+  public void testInt64() {
+    doTestValueWriter(
+      PrimitiveTypeName.INT64,
+      WriterVersion.PARQUET_1_0,
+      true,
+      PlainLongDictionaryValuesWriter.class, PlainValuesWriter.class);
+  }
+
+  @Test
+  public void testInt64_NoDict() {
+    doTestValueWriter(
+      PrimitiveTypeName.INT64,
+      WriterVersion.PARQUET_1_0,
+      false,
+      PlainValuesWriter.class);
+  }
+
+  @Test
+  public void testInt64_V2() {
+    doTestValueWriter(
+      PrimitiveTypeName.INT64,
+      WriterVersion.PARQUET_2_0,
+      true,
+      PlainLongDictionaryValuesWriter.class, DeltaBinaryPackingValuesWriterForLong.class);
+  }
+
+  @Test
+  public void testInt64_V2_NoDict() {
+    doTestValueWriter(
+      PrimitiveTypeName.INT64,
+      WriterVersion.PARQUET_2_0,
+      false,
+      DeltaBinaryPackingValuesWriterForLong.class);
+  }
+
+  @Test
+  public void testInt96() {
+    doTestValueWriter(
+      PrimitiveTypeName.INT96,
+      WriterVersion.PARQUET_1_0,
+      true,
+      PlainFixedLenArrayDictionaryValuesWriter.class, FixedLenByteArrayPlainValuesWriter.class);
+  }
+
+  @Test
+  public void testInt96_NoDict() {
+    doTestValueWriter(
+      PrimitiveTypeName.INT96,
+      WriterVersion.PARQUET_1_0,
+      false,
+      FixedLenByteArrayPlainValuesWriter.class);
+  }
+
+  @Test
+  public void testInt96_V2() {
+    doTestValueWriter(
+      PrimitiveTypeName.INT96,
+      WriterVersion.PARQUET_2_0,
+      true,
+      PlainFixedLenArrayDictionaryValuesWriter.class, FixedLenByteArrayPlainValuesWriter.class);
+  }
+
+  @Test
+  public void testInt96_V2_NoDict() {
+    doTestValueWriter(
+      PrimitiveTypeName.INT96,
+      WriterVersion.PARQUET_2_0,
+      false,
+      FixedLenByteArrayPlainValuesWriter.class);
+  }
+
+  @Test
+  public void testDouble() {
+    doTestValueWriter(
+      PrimitiveTypeName.DOUBLE,
+      WriterVersion.PARQUET_1_0,
+      true,
+      PlainDoubleDictionaryValuesWriter.class, PlainValuesWriter.class);
+  }
+
+  @Test
+  public void testDouble_NoDict() {
+    doTestValueWriter(
+      PrimitiveTypeName.DOUBLE,
+      WriterVersion.PARQUET_1_0,
+      false,
+      PlainValuesWriter.class);
+  }
+
+  @Test
+  public void testDouble_V2() {
+    doTestValueWriter(
+      PrimitiveTypeName.DOUBLE,
+      WriterVersion.PARQUET_2_0,
+      true,
+      PlainDoubleDictionaryValuesWriter.class, PlainValuesWriter.class);
+  }
+
+  @Test
+  public void testDouble_V2_NoDict() {
+    doTestValueWriter(
+      PrimitiveTypeName.DOUBLE,
+      WriterVersion.PARQUET_2_0,
+      false,
+      PlainValuesWriter.class);
+  }
+
+  @Test
+  public void testFloat() {
+    doTestValueWriter(
+      PrimitiveTypeName.FLOAT,
+      WriterVersion.PARQUET_1_0,
+      true,
+      PlainFloatDictionaryValuesWriter.class, PlainValuesWriter.class);
+  }
+
+  @Test
+  public void testFloat_NoDict() {
+    doTestValueWriter(
+      PrimitiveTypeName.FLOAT,
+      WriterVersion.PARQUET_1_0,
+      false,
+      PlainValuesWriter.class);
+  }
+
+  @Test
+  public void testFloat_V2() {
+    doTestValueWriter(
+      PrimitiveTypeName.FLOAT,
+      WriterVersion.PARQUET_2_0,
+      true,
+      PlainFloatDictionaryValuesWriter.class, PlainValuesWriter.class);
+  }
+
+  @Test
+  public void testFloat_V2_NoDict() {
+    doTestValueWriter(
+      PrimitiveTypeName.FLOAT,
+      WriterVersion.PARQUET_2_0,
+      false,
+      PlainValuesWriter.class);
+  }
+
+  private void doTestValueWriter(PrimitiveTypeName typeName, WriterVersion version, boolean enableDictionary, Class<? extends ValuesWriter> expectedValueWriterClass) {
+    ColumnDescriptor mockPath = getMockColumn(typeName);
+    ValuesWriterFactory factory = getDefaultFactory(version, enableDictionary);
+    ValuesWriter writer = factory.newValuesWriter(mockPath);
+
+    validateWriterType(writer, expectedValueWriterClass);
+  }
+
+  private void doTestValueWriter(PrimitiveTypeName typeName, WriterVersion version, boolean enableDictionary, Class<? extends ValuesWriter> initialValueWriterClass, Class<? extends ValuesWriter> fallbackValueWriterClass) {
+    ColumnDescriptor mockPath = getMockColumn(typeName);
+    ValuesWriterFactory factory = getDefaultFactory(version, enableDictionary);
+    ValuesWriter writer = factory.newValuesWriter(mockPath);
+
+    validateFallbackWriter(writer, initialValueWriterClass, fallbackValueWriterClass);
+  }
+
+  private ColumnDescriptor getMockColumn(PrimitiveTypeName typeName) {
+    ColumnDescriptor mockPath = mock(ColumnDescriptor.class);
+    when(mockPath.getType()).thenReturn(typeName);
+    return mockPath;
+  }
+
+  private ValuesWriterFactory getDefaultFactory(WriterVersion writerVersion, boolean enableDictionary) {
+    ValuesWriterFactory factory = new DefaultValuesWriterFactory();
+    ParquetProperties.builder()
+      .withDictionaryEncoding(enableDictionary)
+      .withWriterVersion(writerVersion)
+      .withValuesWriterFactory(factory)
+      .build();
+
+    return factory;
+  }
+
+  private void validateWriterType(ValuesWriter writer, Class<? extends ValuesWriter> valuesWriterClass) {
+    assertTrue("Not instance of: " + valuesWriterClass.getName(), valuesWriterClass.isInstance(writer));
+  }
+
+  private void validateFallbackWriter(ValuesWriter writer, Class<? extends ValuesWriter> initialWriterClass, Class<? extends ValuesWriter> fallbackWriterClass) {
+    validateWriterType(writer, FallbackValuesWriter.class);
+
+    FallbackValuesWriter wr = (FallbackValuesWriter) writer;
+    validateWriterType(wr.initialWriter, initialWriterClass);
+    validateWriterType(wr.fallBackWriter, fallbackWriterClass);
+  }
+}
