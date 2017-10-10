d55a572e5867832f6d5755fcd46101da51a38aa4
Addisu Feyissa <addisu.feyissa@C1159.local>
2017-10-10 16:20:55 -0700

    PARQUET-1133 Add int96 support by returning bytearray, Skip originalType comparison for map types when originalType is null
    - PigSchemaConverter: Added a null check before comparing a mapKeyValueType's original type with the static constant
    - PigSchemaConverter: Changed the handling of int96 types - return bytearray instead of rejecting input
    - PigSchemaConverterTest: Added unit tests for int96 conversion and handling map entries without original type specified
    
    Author: Addisu Feyissa <addisu.feyissa@C1159.local>
    
    Closes #422 from adisu-feyissa/hotfix/remove_originalType_check_for_maps_and_add_int96_support and squashes the following commits:
    
    e6fa3444 [Addisu Feyissa] - PigSchemaConverter: Added a null check before comparing a mapKeyValueType's original type with the static constant - PigSchemaConverter: Changed the handling of int96 types - return bytearray instead of rejecting input - PigSchemaConverTest: Added unit tests for int96 conversion and handling map entries without original type specified

--- parquet-pig/src/main/java/org/apache/parquet/pig/PigSchemaConverter.java
+++ parquet-pig/src/main/java/org/apache/parquet/pig/PigSchemaConverter.java
@@ -239,7 +239,8 @@ public class PigSchemaConverter {
       @Override
       public FieldSchema convertINT96(PrimitiveTypeName primitiveTypeName)
           throws FrontendException {
-        throw new FrontendException("NYI");
+        LOG.warn("Converting type " + primitiveTypeName + " to bytearray");
+        return new FieldSchema(fieldName, null, DataType.BYTEARRAY);
       }
 
       @Override
@@ -283,7 +284,7 @@ public class PigSchemaConverter {
         }
         GroupType mapKeyValType = parquetGroupType.getType(0).asGroupType();
         if (!mapKeyValType.isRepetition(Repetition.REPEATED) ||
-            !mapKeyValType.getOriginalType().equals(OriginalType.MAP_KEY_VALUE) ||
+            (mapKeyValType.getOriginalType() != null && !mapKeyValType.getOriginalType().equals(OriginalType.MAP_KEY_VALUE)) ||
             mapKeyValType.getFieldCount()!=2) {
           throw new SchemaConversionException("Invalid map type " + parquetGroupType);
         }
