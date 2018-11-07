b6fd45ee08e08b490c35825953b51cfd8fb1b072
nandorKollar <nandorKollar@users.noreply.github.com>
2018-11-07 11:19:10 +0100

    PARQUET-1305: Backward incompatible change introduced in 1.8 (#483)

--- parquet-thrift/src/main/java/org/apache/parquet/thrift/struct/ThriftType.java
+++ parquet-thrift/src/main/java/org/apache/parquet/thrift/struct/ThriftType.java
@@ -1,4 +1,4 @@
-/* 
+/*
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
  * distributed with this work for additional information
@@ -6,9 +6,9 @@
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
@@ -230,7 +230,7 @@ public abstract class ThriftType {
     public StructType(@JsonProperty("children") List<ThriftField> children,
                       @JsonProperty("structOrUnionType") StructOrUnionType structOrUnionType) {
       super(STRUCT);
-      this.structOrUnionType = structOrUnionType == null ? StructOrUnionType.UNKNOWN : structOrUnionType;
+      this.structOrUnionType = structOrUnionType == null ? StructOrUnionType.STRUCT : structOrUnionType;
       this.children = children;
       int maxId = 0;
       if (children != null) {
