58edbd78ca93b360daee791376e4b1bcf01c5f61
nandorKollar <nandorKollar@users.noreply.github.com>
2019-01-07 16:46:06 +0100

    PARQUET-1478: Can't read spec compliant, 3-level lists via parquet-proto (#578)

--- parquet-protobuf/src/test/java/org/apache/parquet/proto/utils/WriteUsingMR.java
+++ parquet-protobuf/src/test/java/org/apache/parquet/proto/utils/WriteUsingMR.java
@@ -1,4 +1,4 @@
-/* 
+/*
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
  * distributed with this work for additional information
@@ -6,9 +6,9 @@
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
@@ -33,7 +33,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
@@ -55,7 +54,7 @@ public class WriteUsingMR {
   }
 
   public WriteUsingMR(Configuration conf) {
-    this.conf = new Configuration();
+    this.conf = conf;
   }
 
   public Configuration getConfiguration() {
