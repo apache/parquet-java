4c3c2a53e86275de97c66f1b1b8b9c5ed7bd6953
Zoltan Ivanfi <zi@cloudera.com>
2019-01-09 17:36:47 +0100

    Move check to its final place.

--- dev/travis-before-install.sh
+++ dev/travis-before-install.sh
@@ -5,11 +5,6 @@ sudo apt-get install -qq build-essential pv autoconf automake libtool curl make 
    g++ unzip libboost-dev libboost-test-dev libboost-program-options-dev \
    libevent-dev automake libtool flex bison pkg-config g++ libssl-dev xmlstarlet
 date
-branch_specific_script="dev/travis-before-install-${TRAVIS_BRANCH}.sh"
-if [[ -e "$branch_specific_script" ]]
-then
-  . "$branch_specific_script"
-fi
 mkdir protobuf_install
 pushd protobuf_install
 wget https://github.com/google/protobuf/archive/v3.5.1.tar.gz -O protobuf-3.5.1.tar.gz
@@ -31,4 +26,9 @@ chmod +x ./configure
 ./configure --disable-gen-erl --disable-gen-hs --without-ruby --without-haskell --without-erlang --without-php --without-nodejs
 sudo make install
 cd ..
+branch_specific_script="dev/travis-before-install-${TRAVIS_BRANCH}.sh"
+if [[ -e "$branch_specific_script" ]]
+then
+  . "$branch_specific_script"
+fi
 date
