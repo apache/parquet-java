cad2d83b0fc4eb29e602a8fb576b756f990605f6
Zoltan Ivanfi <zi@cloudera.com>
2019-01-09 17:54:02 +0100

    Added comments.

--- dev/travis-before-install-master.sh
+++ dev/travis-before-install-master.sh
@@ -1,8 +1,10 @@
-shopt -s globstar
 fail_the_build=
 reduced_pom="$(tempfile)"
+shopt -s globstar # Enables ** to match files in subdirectories recursively
 for pom in **/pom.xml
 do
+  # Removes the project/version and project/parent/version elements, because
+  # those are allowed to have SNAPSHOT in them. Also removes comments.
   xmlstarlet ed -N pom='http://maven.apache.org/POM/4.0.0' \
              -d '/pom:project/pom:version|/pom:project/pom:parent/pom:version|//comment()' "$pom" > "$reduced_pom"
   if grep -q SNAPSHOT "$reduced_pom"
@@ -13,6 +15,9 @@ do
       fail_the_build=YES
     fi
     printf "\nOffending POM file: %s\nOffending content:\n" "$pom"
+    # Removes every element that does not have SNAPSHOT in it or its
+    # descendants. As a result, we get a skeleton of the POM file with only the
+    # offending parts.
     xmlstarlet ed -d "//*[count((.|.//*)[contains(text(), 'SNAPSHOT')]) = 0]" "$reduced_pom"
   fi
 done
