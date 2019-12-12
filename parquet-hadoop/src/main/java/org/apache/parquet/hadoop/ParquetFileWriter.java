565f5e5698ee61d1c051d5c3a9a9c38785b0b8ae
William Montaz <w.montaz@criteo.com>
2019-12-12 11:38:14 +0100

    [PARQUET-870] ParquetFileWriter.appendFile does not close file handle - resource leak

--- parquet-hadoop/src/main/java/org/apache/parquet/hadoop/ParquetFileWriter.java
+++ parquet-hadoop/src/main/java/org/apache/parquet/hadoop/ParquetFileWriter.java
@@ -1,4 +1,4 @@
-/* 
+/*
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
  * distributed with this work for additional information
@@ -6,9 +6,9 @@
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
@@ -512,11 +512,15 @@ public class ParquetFileWriter {
    */
   @Deprecated
   public void appendFile(Configuration conf, Path file) throws IOException {
-    ParquetFileReader.open(conf, file).appendTo(this);
+    try(ParquetFileReader reader = ParquetFileReader.open(conf, file)){
+      reader.appendTo(this);
+    }
   }
 
   public void appendFile(InputFile file) throws IOException {
-    ParquetFileReader.open(file).appendTo(this);
+    try(ParquetFileReader reader = ParquetFileReader.open(file)){
+      reader.appendTo(this);
+    }
   }
 
   /**
