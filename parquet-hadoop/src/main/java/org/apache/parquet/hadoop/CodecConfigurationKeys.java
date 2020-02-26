145f94ce42a4f99085d62672b08c8ba7f96f92fc
Dong <xin.dong@intel.com>
2020-02-26 09:57:55 +0800

    fix build error

--- parquet-hadoop/src/main/java/org/apache/parquet/hadoop/CodecConfigurationKeys.java
+++ parquet-hadoop/src/main/java/org/apache/parquet/hadoop/CodecConfigurationKeys.java
@@ -1,5 +1,7 @@
 package org.apache.parquet.hadoop;
 
+import org.apache.hadoop.fs.CommonConfigurationKeys;
+
 public class CodecConfigurationKeys extends CommonConfigurationKeys{
   /** Customized Compression Codec name */
   public static final String CUSTOMIZED_COMPRESSION_CODEC_NAME =
