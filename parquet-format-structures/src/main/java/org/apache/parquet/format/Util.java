b071e19336af33319499962e0da62b35e6822a9d
Gidon Gershinsky <gg5070@gmail.com>
2020-03-17 11:47:02 +0200

    bloom filter encryption in format-structures

--- parquet-format-structures/src/main/java/org/apache/parquet/format/Util.java
+++ parquet-format-structures/src/main/java/org/apache/parquet/format/Util.java
@@ -96,14 +96,6 @@ public class Util {
     return read(from, new OffsetIndex(), decryptor, AAD);
   }
 
-  public static BloomFilterHeader readBloomFilterHeader(InputStream from) throws IOException {
-    return read(from, new BloomFilterHeader());
-  }
-
-  public static void writeBloomFilterHeader(BloomFilterHeader header, OutputStream out) throws IOException {
-    write(header, out);
-  }
-
   public static void writePageHeader(PageHeader pageHeader, OutputStream to) throws IOException {
     writePageHeader(pageHeader, to, null, null);
   }
