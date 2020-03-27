c2edf3ddddf51c9b918e7b155560b1d37f39dd4f
shangxinli <31421745+shangxinli@users.noreply.github.com>
2020-03-27 08:18:58 -0700

    Update parquet-hadoop/src/main/java/org/apache/parquet/crypto/DecryptionPropertiesFactory.java
    Co-Authored-By: Gabor Szadovszky <gabor@apache.org>

--- parquet-hadoop/src/main/java/org/apache/parquet/crypto/DecryptionPropertiesFactory.java
+++ parquet-hadoop/src/main/java/org/apache/parquet/crypto/DecryptionPropertiesFactory.java
@@ -37,7 +37,7 @@ import org.slf4j.LoggerFactory;
  *   2. Set configuration of "parquet.crypto.factory.class" with the fully qualified name of this class.
  *      For example, we can set the configuration in SparkSession as below.
  *         SparkSession spark = SparkSession
- *                     .config("parquet.encryption.factory.class",
+ *                     .config("parquet.crypto.factory.class",
  *                     "xxx.xxx.DecryptionPropertiesClassLoaderImpl")
  *
  * The implementation of this interface will be instantiated by {@link #loadFactory(Configuration)}.
