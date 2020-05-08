7066e85decab6196b5d4c3966d4b1d501cc01735
Santosh Lade <lssatvik@gmail.com>
2020-05-08 12:16:56 +0530

    Add Configuration import
    Added missing import of class Configuration

--- parquet-protobuf/src/main/java/org/apache/parquet/proto/ProtoParquetWriter.java
+++ parquet-protobuf/src/main/java/org/apache/parquet/proto/ProtoParquetWriter.java
@@ -25,6 +25,7 @@ import org.apache.parquet.hadoop.ParquetWriter;
 import org.apache.parquet.hadoop.api.WriteSupport;
 import org.apache.parquet.hadoop.metadata.CompressionCodecName;
 import org.apache.parquet.io.OutputFile;
+import org.apache.hadoop.conf.Configuration;
 
 import java.io.IOException;
 
