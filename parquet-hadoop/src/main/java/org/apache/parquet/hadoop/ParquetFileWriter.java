7a8cc10ec25eac5d3d21ec96129951fcd63a0a51
ggershinsky <ggershinsky@users.noreply.github.com>
2020-05-21 17:53:09 +0300

    Update parquet-hadoop/src/main/java/org/apache/parquet/hadoop/ParquetFileWriter.java
    Co-authored-by: Gabor Szadovszky <gabor@apache.org>

--- parquet-hadoop/src/main/java/org/apache/parquet/hadoop/ParquetFileWriter.java
+++ parquet-hadoop/src/main/java/org/apache/parquet/hadoop/ParquetFileWriter.java
@@ -103,7 +103,7 @@ public class ParquetFileWriter {
   public static final String MAGIC_STR = "PAR1";
   public static final byte[] MAGIC = MAGIC_STR.getBytes(StandardCharsets.US_ASCII);
   public static final String EF_MAGIC_STR = "PARE";
-  public static final byte[] EFMAGIC = EF_MAGIC_STR.getBytes(Charset.forName("ASCII"));
+  public static final byte[] EFMAGIC = EF_MAGIC_STR.getBytes(StandardCharsets.US_ASCII);
   public static final String PARQUET_COMMON_METADATA_FILE = "_common_metadata";
   public static final int CURRENT_VERSION = 1;
 
