883ff4152820a4b37795b4980f9f09dc8b42208f
David Mollitor <dmollitor@apache.org>
2020-10-02 09:58:56 -0400

    PARQUET-1918: Avoid Copy of Bytes in Protobuf BinaryWriter

--- parquet-protobuf/src/main/java/org/apache/parquet/proto/ProtoWriteSupport.java
+++ parquet-protobuf/src/main/java/org/apache/parquet/proto/ProtoWriteSupport.java
@@ -546,7 +546,7 @@ public class ProtoWriteSupport<T extends MessageOrBuilder> extends WriteSupport<
     @Override
     final void writeRawValue(Object value) {
       ByteString byteString = (ByteString) value;
-      Binary binary = Binary.fromConstantByteArray(byteString.toByteArray());
+      Binary binary = Binary.fromConstantByteBuffer(byteString.asReadOnlyByteBuffer());
       recordConsumer.addBinary(binary);
     }
   }
