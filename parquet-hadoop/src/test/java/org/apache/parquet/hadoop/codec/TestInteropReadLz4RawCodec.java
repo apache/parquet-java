c12926d632c80af88ac31c0775b7595c11ee7969
Gang Wu <ustcwg@gmail.com>
2022-10-12 10:18:43 +0800

    change interop test to read from resource

--- parquet-hadoop/src/test/java/org/apache/parquet/hadoop/codec/TestInteropReadLz4RawCodec.java
+++ parquet-hadoop/src/test/java/org/apache/parquet/hadoop/codec/TestInteropReadLz4RawCodec.java
@@ -19,75 +19,42 @@
 
 package org.apache.parquet.hadoop.codec;
 
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
-import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.fs.FSDataOutputStream;
-import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.parquet.example.data.Group;
 import org.apache.parquet.hadoop.ParquetReader;
 import org.apache.parquet.hadoop.example.GroupReadSupport;
 import org.junit.Test;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
+import java.net.URISyntaxException;
 
 import static org.junit.Assert.assertEquals;
 
 public class TestInteropReadLz4RawCodec {
 
-  // The link includes a reference to a specific commit. To take a newer version - update this link.
-  private static final String PARQUET_TESTING_REPO = "https://github.com/apache/parquet-testing/raw/19fcd4d/data/";
-  private static String PARQUET_TESTING_PATH = "target/parquet-testing/data";
-  private static String SIMPLE_FILE = "lz4_raw_compressed.parquet";
-  private static String LARGER_FILE = "lz4_raw_compressed_larger.parquet";
-
-  private static final Logger LOG = LoggerFactory.getLogger(TestInteropReadLz4RawCodec.class);
-  private OkHttpClient httpClient = new OkHttpClient();
+  private static String SIMPLE_FILE = "/lz4_raw_compressed.parquet";
+  private static String LARGER_FILE = "/lz4_raw_compressed_larger.parquet";
 
   @Test
-  public void testInteropReadLz4RawParquetFiles() throws IOException {
-    Path rootPath = new Path(PARQUET_TESTING_PATH);
-    LOG.info("======== testInteropReadLz4RawParquetFiles {} ========", rootPath.toString());
-
+  public void testInteropReadSimpleLz4RawParquetFile() throws IOException {
     // Test simple parquet file with lz4 raw compressed
-    Path simpleFile = downloadInteropFiles(rootPath, SIMPLE_FILE, httpClient);
+    Path simpleFile = createPathFromCP(SIMPLE_FILE);
     readParquetFile(simpleFile, 4);
+  }
 
+  @Test
+  public void testInteropReadLargerLz4RawParquetFile() throws IOException {
     // Test larger parquet file with lz4 raw compressed
-    Path largerFile = downloadInteropFiles(rootPath, LARGER_FILE, httpClient);
+    Path largerFile = createPathFromCP(LARGER_FILE);
     readParquetFile(largerFile, 10000);
   }
 
-  private Path downloadInteropFiles(Path rootPath, String fileName, OkHttpClient httpClient) throws IOException {
-    LOG.info("Download interop files if needed");
-    Configuration conf = new Configuration();
-    FileSystem fs = rootPath.getFileSystem(conf);
-    LOG.info(rootPath + " exists?: " + fs.exists(rootPath));
-    if (!fs.exists(rootPath)) {
-      LOG.info("Create folder for interop files: " + rootPath);
-      if (!fs.mkdirs(rootPath)) {
-        throw new IOException("Cannot create path " + rootPath);
-      }
+  private static Path createPathFromCP(String path) {
+    try {
+      return new Path(TestInteropReadLz4RawCodec.class.getResource(path).toURI());
+    } catch (URISyntaxException e) {
+      throw new RuntimeException(e);
     }
-
-    Path file = new Path(rootPath, fileName);
-    if (!fs.exists(file)) {
-      String downloadUrl = PARQUET_TESTING_REPO + fileName;
-      LOG.info("Download interop file: " + downloadUrl);
-      Request request = new Request.Builder().url(downloadUrl).build();
-      Response response = httpClient.newCall(request).execute();
-      if (!response.isSuccessful()) {
-        throw new IOException("Failed to download file: " + response);
-      }
-      try (FSDataOutputStream fdos = fs.create(file)) {
-        fdos.write(response.body().bytes());
-      }
-    }
-    return file;
   }
 
   private void readParquetFile(Path filePath, int expectedNumRows) throws IOException {
