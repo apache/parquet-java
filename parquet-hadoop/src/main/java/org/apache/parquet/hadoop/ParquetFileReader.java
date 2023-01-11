e14d18f2ac98a58bf727aa2cce678207446167f4
Gang Wu <ustcwg@gmail.com>
2023-01-11 13:06:19 +0800

    print file path in the warning

--- parquet-hadoop/src/main/java/org/apache/parquet/hadoop/ParquetFileReader.java
+++ parquet-hadoop/src/main/java/org/apache/parquet/hadoop/ParquetFileReader.java
@@ -929,7 +929,7 @@ public class ParquetFileReader implements Closeable {
     try {
       rowGroup = internalReadRowGroup(currentBlock);
     } catch (ParquetEmptyBlockException e) {
-      LOG.warn("Read empty block at index {}", currentBlock);
+      LOG.warn("Read empty block at index {} from {}", currentBlock, getFile());
       advanceToNextBlock();
       return readNextRowGroup();
     }
@@ -1044,7 +1044,7 @@ public class ParquetFileReader implements Closeable {
     }
     BlockMetaData block = blocks.get(currentBlock);
     if (block.getRowCount() == 0L) {
-      LOG.warn("Read empty block at index {}", currentBlock);
+      LOG.warn("Read empty block at index {} from {}", currentBlock, getFile());
       // Skip the empty block
       advanceToNextBlock();
       return readNextFilteredRowGroup();
