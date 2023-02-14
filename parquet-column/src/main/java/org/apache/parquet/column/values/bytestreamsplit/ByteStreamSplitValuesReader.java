4584fe5cc18253efe07f2d404be2ea5b3956b08b
Gang Wu <ustcwg@gmail.com>
2023-02-14 09:34:32 +0800

    add an upper bound check

--- parquet-column/src/main/java/org/apache/parquet/column/values/bytestreamsplit/ByteStreamSplitValuesReader.java
+++ parquet-column/src/main/java/org/apache/parquet/column/values/bytestreamsplit/ByteStreamSplitValuesReader.java
@@ -1,4 +1,4 @@
-/* 
+/*
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
  * distributed with this work for additional information
@@ -6,9 +6,9 @@
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
@@ -68,6 +68,14 @@ public abstract class ByteStreamSplitValuesReader extends ValuesReader {
       throw new ParquetDecodingException(errorMessage);
     }
     this.valuesCount = stream.available() / elementSizeInBytes;
+
+    // The input valuesCount includes number of nulls. It is used for an upperbound check only.
+    if (valuesCount < this.valuesCount) {
+      String errorMessage = String.format(
+              "Invalid ByteStreamSplit stream, num values upper bound (w/ nulls): %d, num encoded values: %d",
+              valuesCount, this.valuesCount);
+      throw new ParquetDecodingException(errorMessage);
+    }
 
     // Allocate buffer for all of the byte stream data.
     final int totalSizeInBytes = stream.available();
