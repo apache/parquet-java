249d2182e6366c8738e8f3843168e028527258de
chenliang.lu <marssss2929@gmail.com>
2023-03-19 18:29:32 +0800

    remove unnecessary code

--- parquet-hadoop/src/test/java/org/apache/parquet/hadoop/TestParquetWriter.java
+++ parquet-hadoop/src/test/java/org/apache/parquet/hadoop/TestParquetWriter.java
@@ -386,16 +386,6 @@ public class TestParquetWriter {
     }
   }
 
-  static final int tableSizeFor(int cap) {
-    int n = cap - 1;
-    n |= n >>> 1;
-    n |= n >>> 2;
-    n |= n >>> 4;
-    n |= n >>> 8;
-    n |= n >>> 16;
-    return n + 1;
-  }
-
   @Test
   public void testParquetFileWritesExpectedNumberOfBlocks() throws IOException {
     testParquetFileNumberOfBlocks(ParquetProperties.DEFAULT_MINIMUM_RECORD_COUNT_FOR_CHECK,
