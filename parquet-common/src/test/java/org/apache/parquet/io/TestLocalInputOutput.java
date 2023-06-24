9f309a2ca0f253eba7e43116c1b3a57d13fa262d
amousavigourabi <28668597+amousavigourabi@users.noreply.github.com>
2023-06-24 20:37:50 +0200

    Use temp files for tests

--- parquet-common/src/test/java/org/apache/parquet/io/TestLocalInputOutput.java
+++ parquet-common/src/test/java/org/apache/parquet/io/TestLocalInputOutput.java
@@ -18,29 +18,29 @@
  */
 package org.apache.parquet.io;
 
-import org.junit.Test;
-
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.FileAlreadyExistsException;
-import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThrows;
 
 public class TestLocalInputOutput {
 
-  Path pathFileExists = Paths.get("src/test/resources/disk_output_file_create_overwrite.parquet");
-  Path pathNewFile = Paths.get("src/test/resources/disk_output_file_create.parquet");
-
   @Test
   public void outputFileOverwritesFile() throws IOException {
-    OutputFile write = new LocalOutputFile(pathFileExists);
+    Path path = Paths.get(createTempFile().getPath());
+    OutputFile write = new LocalOutputFile(path);
     try (PositionOutputStream stream = write.createOrOverwrite(512)) {
       stream.write(124);
     }
-    InputFile read = new LocalInputFile(pathFileExists);
+    try (PositionOutputStream stream = write.createOrOverwrite(512)) {
+      stream.write(124);
+    }
+    InputFile read = new LocalInputFile(path);
     try (SeekableInputStream stream = read.newStream()) {
       assertEquals(stream.read(), 124);
       assertEquals(stream.read(), -1);
@@ -48,36 +48,44 @@ public class TestLocalInputOutput {
   }
 
   @Test
-  public void outputFileCreateFailsAsFileAlreadyExists() {
-    OutputFile write = new LocalOutputFile(pathFileExists);
+  public void outputFileCreateFailsAsFileAlreadyExists() throws IOException {
+    Path path = Paths.get(createTempFile().getPath());
+    OutputFile write = new LocalOutputFile(path);
     assertThrows(FileAlreadyExistsException.class, () -> write.create(512));
   }
 
   @Test
   public void outputFileCreatesFileWithOverwrite() throws IOException {
-    OutputFile write = new LocalOutputFile(pathNewFile);
+    Path path = Paths.get(createTempFile().getPath());
+    OutputFile write = new LocalOutputFile(path);
     try (PositionOutputStream stream = write.createOrOverwrite(512)) {
       stream.write(255);
     }
-    InputFile read = new LocalInputFile(pathNewFile);
+    InputFile read = new LocalInputFile(path);
     try (SeekableInputStream stream = read.newStream()) {
       assertEquals(stream.read(), 255);
       assertEquals(stream.read(), -1);
     }
-    Files.delete(pathNewFile);
   }
 
   @Test
   public void outputFileCreatesFile() throws IOException {
-    OutputFile write = new LocalOutputFile(pathNewFile);
+    Path path = Paths.get(createTempFile().getPath());
+    OutputFile write = new LocalOutputFile(path);
     try (PositionOutputStream stream = write.createOrOverwrite(512)) {
       stream.write(2);
     }
-    InputFile read = new LocalInputFile(pathNewFile);
+    InputFile read = new LocalInputFile(path);
     try (SeekableInputStream stream = read.newStream()) {
       assertEquals(stream.read(), 2);
       assertEquals(stream.read(), -1);
     }
-    Files.delete(pathNewFile);
+  }
+
+  private File createTempFile() throws IOException {
+    File tmp = File.createTempFile(getClass().getSimpleName(), ".tmp");
+    tmp.deleteOnExit();
+    tmp.delete();
+    return tmp;
   }
 }
