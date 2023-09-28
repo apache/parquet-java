a1bacf88d33c4ddbb5a131e13b6a1b01bf6d63e9
Fokko Driesprong <fokko@apache.org>
2023-09-28 10:22:22 +0200

    Remove old setting for cascading (#1144)

--- parquet-hadoop/src/main/java/org/apache/parquet/hadoop/metadata/ParquetMetadata.java
+++ parquet-hadoop/src/main/java/org/apache/parquet/hadoop/metadata/ParquetMetadata.java
@@ -38,12 +38,6 @@ public class ParquetMetadata {
 
   private static final ObjectMapper objectMapper = new ObjectMapper();
 
-  // Enable FAIL_ON_EMPTY_BEANS on objectmapper. Without this feature parquet-casdacing tests fail,
-  // because LogicalTypeAnnotation implementations are classes without any property.
-  static {
-    objectMapper.disable(SerializationFeature.FAIL_ON_EMPTY_BEANS);
-  }
-
   /**
    * @param parquetMetaData an instance of parquet metadata to convert
    * @return the json representation
