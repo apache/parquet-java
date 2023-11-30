778dfa6ec1ee4641fddca3ded380f54f5dd0152c
Fokko Driesprong <fokko@tabular.io>
2023-11-30 06:16:50 -0800

    Remove unused imports

--- parquet-common/src/main/java/org/apache/parquet/glob/GlobExpander.java
+++ parquet-common/src/main/java/org/apache/parquet/glob/GlobExpander.java
@@ -19,7 +19,6 @@
 package org.apache.parquet.glob;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import org.apache.parquet.glob.GlobNode.Atom;
