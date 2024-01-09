b490e58aa8fc9f0a7fd4ebef3b7c43877bf491ae
Rathin Bhargava <rathin.bhargava@databricks.com>
2024-01-09 09:50:06 +0100

    Run formatting checks

--- parquet-hadoop/src/test/java/org/apache/parquet/hadoop/codec/TestNonBlockedDecompressorStream.java
+++ parquet-hadoop/src/test/java/org/apache/parquet/hadoop/codec/TestNonBlockedDecompressorStream.java
@@ -18,15 +18,14 @@
  */
 package org.apache.parquet.hadoop.codec;
 
-import org.apache.hadoop.io.compress.Decompressor;
-import org.junit.Test;
-import org.junit.Assert;
-import org.mockito.Mockito;
+import static org.mockito.Mockito.when;
 
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
-
-import static org.mockito.Mockito.when;
+import org.apache.hadoop.io.compress.Decompressor;
+import org.junit.Assert;
+import org.junit.Test;
+import org.mockito.Mockito;
 
 public class TestNonBlockedDecompressorStream {
 
@@ -35,12 +34,12 @@ public class TestNonBlockedDecompressorStream {
     try {
       // Create a mock Decompressor that returns 0 when decompress is called.
       Decompressor mockDecompressor = Mockito.mock(Decompressor.class);
-      when(mockDecompressor.decompress(Mockito.any(byte[].class), Mockito.anyInt(),
-          Mockito.anyInt())).thenReturn(0);
+      when(mockDecompressor.decompress(Mockito.any(byte[].class), Mockito.anyInt(), Mockito.anyInt()))
+          .thenReturn(0);
 
       // Create a NonBlockedDecompressorStream with the mock Decompressor.
-      NonBlockedDecompressorStream decompressorStream = new NonBlockedDecompressorStream(
-          new ByteArrayInputStream(new byte[0]), mockDecompressor, 1024);
+      NonBlockedDecompressorStream decompressorStream =
+          new NonBlockedDecompressorStream(new ByteArrayInputStream(new byte[0]), mockDecompressor, 1024);
 
       // Attempt to read from the stream, which should trigger the IOException.
       decompressorStream.read(new byte[1024], 0, 1024);
@@ -48,8 +47,8 @@ public class TestNonBlockedDecompressorStream {
       // If no exception is thrown, then the test fails.
       Assert.fail("Expected an IOException to be thrown");
     } catch (IOException e) {
-      Assert.assertEquals("Zero bytes read during decompression, suggesting a potential file corruption.",
-          e.getMessage());
+      Assert.assertEquals(
+          "Zero bytes read during decompression, suggesting a potential file corruption.", e.getMessage());
     }
   }
 }
