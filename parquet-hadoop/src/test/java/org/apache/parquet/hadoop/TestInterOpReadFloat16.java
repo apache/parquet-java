f0604cf70cbfd6c079a406254ceba82f2db03e21
Antoine Pitrou <antoine@python.org>
2024-01-19 11:02:51 +0100

    Disable wildcard imports

--- parquet-hadoop/src/test/java/org/apache/parquet/hadoop/TestInterOpReadFloat16.java
+++ parquet-hadoop/src/test/java/org/apache/parquet/hadoop/TestInterOpReadFloat16.java
@@ -19,7 +19,9 @@
 
 package org.apache.parquet.hadoop;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
 import java.io.IOException;
 import org.apache.hadoop.conf.Configuration;
