dfa1a0618b6391547b6e97eafcbfd494282a8b4a
Rathin Bhargava <rathin.bhargava@databricks.com>
2024-01-28 16:39:48 +0100

    Address comments

--- parquet-hadoop/src/test/java/org/apache/parquet/hadoop/codec/TestCompressionCodec.java
+++ parquet-hadoop/src/test/java/org/apache/parquet/hadoop/codec/TestCompressionCodec.java
@@ -18,7 +18,9 @@
  */
 package org.apache.parquet.hadoop.codec;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
 import static org.mockito.Mockito.when;
 
 import java.io.ByteArrayInputStream;
