368840ee655ca1d07d155d39a28e465d67cffbbd
Fokko Driesprong <fokko@tabular.io>
2024-04-23 13:57:01 +0200

    PARQUET-2462: Bump to Thrift 0.20.0

--- dev/ci-before_install.sh
+++ dev/ci-before_install.sh
@@ -20,7 +20,7 @@
 # This script gets invoked by the CI system in a "before install" step
 ################################################################################
 
-export THRIFT_VERSION=0.19.0
+export THRIFT_VERSION=0.20.0
 
 set -e
 date
