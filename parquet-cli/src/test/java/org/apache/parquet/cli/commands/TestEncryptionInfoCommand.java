9fe010270efbd8e479db66b5f1f97dcd0dd6a2cd
arnavb <arnavb@uber.com>
2025-08-27 05:41:37 +0000

    update

--- parquet-cli/src/test/java/org/apache/parquet/cli/commands/TestEncryptionInfoCommand.java
+++ parquet-cli/src/test/java/org/apache/parquet/cli/commands/TestEncryptionInfoCommand.java
@@ -22,53 +22,64 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
-import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
-import java.io.PrintStream;
 import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.Base64;
 import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Set;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.parquet.ParquetReadOptions;
 import org.apache.parquet.column.ParquetProperties;
-import org.apache.parquet.crypto.ColumnEncryptionProperties;
-import org.apache.parquet.crypto.DecryptionKeyRetriever;
+import org.apache.parquet.crypto.DecryptionPropertiesFactory;
+import org.apache.parquet.crypto.EncryptionPropertiesFactory;
 import org.apache.parquet.crypto.FileDecryptionProperties;
 import org.apache.parquet.crypto.FileEncryptionProperties;
-import org.apache.parquet.crypto.KeyAccessDeniedException;
 import org.apache.parquet.crypto.ParquetCryptoRuntimeException;
+import org.apache.parquet.crypto.keytools.PropertiesDrivenCryptoFactory;
 import org.apache.parquet.example.data.Group;
 import org.apache.parquet.example.data.simple.SimpleGroupFactory;
 import org.apache.parquet.hadoop.ParquetFileReader;
+import org.apache.parquet.hadoop.ParquetFileWriter;
 import org.apache.parquet.hadoop.ParquetWriter;
 import org.apache.parquet.hadoop.example.ExampleParquetWriter;
 import org.apache.parquet.hadoop.example.GroupWriteSupport;
-import org.apache.parquet.hadoop.metadata.ColumnPath;
 import org.apache.parquet.hadoop.metadata.CompressionCodecName;
 import org.apache.parquet.hadoop.metadata.ParquetMetadata;
 import org.apache.parquet.hadoop.util.HadoopInputFile;
 import org.apache.parquet.io.InputFile;
 import org.apache.parquet.schema.MessageType;
+import org.apache.parquet.schema.PrimitiveType;
 import org.apache.parquet.schema.Types;
 import org.junit.Before;
 import org.junit.Test;
-import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+/**
+ * This test checks the CLI functionality for encryption info.
+ */
 public class TestEncryptionInfoCommand extends ParquetFileTest {
 
-  private static final byte[] FOOTER_KEY = "0123456789012345".getBytes();
-  private static final byte[] COLUMN_KEY = "1234567890123450".getBytes();
+  private static final byte[] FOOTER_KEY = "0123456789012345".getBytes(StandardCharsets.UTF_8);
+  private static final byte[] COLUMN_KEY = "1234567890123450".getBytes(StandardCharsets.UTF_8);
+
   private static final String FOOTER_KEY_ID = "kf";
   private static final String COLUMN_KEY_ID = "kc";
+
+  private static final String FACTORY_CLASS = "org.apache.parquet.crypto.keytools.PropertiesDrivenCryptoFactory";
+  private static final String MOCK_KMS_CLASS = "org.apache.parquet.crypto.keytools.mocks.InMemoryKMS";
 
   private File unencryptedFile;
   private File columnEncryptedFile;
   private File footerEncryptedFile;
+
+  private enum EncMode {
+    NONE, // No encryption
+    COLUMN_AND_FOOTER, // Both column and footer encrypted
+    COLUMN_PLAINTEXT_FOOTER // Column encrypted, footer plaintext (legacy compatible)
+  }
 
   @Before
   public void setUp() throws IOException {
@@ -77,71 +88,35 @@ public class TestEncryptionInfoCommand extends ParquetFileTest {
   }
 
   private void createTestFiles() throws IOException {
-    unencryptedFile = createParquetFile("unencrypted.parquet", null);
-
-    FileEncryptionProperties columnEncProps = createColumnEncryptionProperties();
-    columnEncryptedFile = createParquetFile("column_encrypted.parquet", columnEncProps);
-
-    FileEncryptionProperties footerEncProps = createFooterEncryptionProperties();
-    footerEncryptedFile = createParquetFile("footer_encrypted.parquet", footerEncProps);
+    unencryptedFile = createParquetFile("unencrypted.parquet", EncMode.NONE);
+    columnEncryptedFile = createParquetFile("column_encrypted.parquet", EncMode.COLUMN_AND_FOOTER);
+    footerEncryptedFile = createParquetFile("footer_encrypted.parquet", EncMode.COLUMN_PLAINTEXT_FOOTER);
   }
 
-  private FileEncryptionProperties createColumnEncryptionProperties() {
-    String columnKeyMetadata = "{\"key\":\"" + COLUMN_KEY_ID + "\"}";
-    String footerKeyMetadata = "{\"key\":\"" + FOOTER_KEY_ID + "\"}";
-
-    ColumnEncryptionProperties columnProps = ColumnEncryptionProperties.builder("ssn")
-        .withKey(COLUMN_KEY)
-        .withKeyMetaData(columnKeyMetadata.getBytes(StandardCharsets.UTF_8))
-        .build();
-
-    Map<ColumnPath, ColumnEncryptionProperties> columnMap = new HashMap<>();
-    columnMap.put(columnProps.getPath(), columnProps);
-
-    return FileEncryptionProperties.builder(FOOTER_KEY)
-        .withFooterKeyMetadata(footerKeyMetadata.getBytes(StandardCharsets.UTF_8))
-        .withEncryptedColumns(columnMap)
-        .build();
-  }
-
-  private FileEncryptionProperties createFooterEncryptionProperties() {
-    // Use JSON metadata format that PropertiesDrivenCryptoFactory expects
-    String footerKeyMetadata = "{\"key\":\"" + FOOTER_KEY_ID + "\"}";
-
-    return FileEncryptionProperties.builder(FOOTER_KEY)
-        .withFooterKeyMetadata(footerKeyMetadata.getBytes(StandardCharsets.UTF_8))
-        .build();
-  }
-
-  private FileDecryptionProperties createDecryptionProperties() {
-    DecryptionKeyRetriever keyRetriever = new DecryptionKeyRetriever() {
-      @Override
-      public byte[] getKey(byte[] keyMetaData) throws KeyAccessDeniedException, ParquetCryptoRuntimeException {
-        String keyID = new String(keyMetaData);
-        if (FOOTER_KEY_ID.equals(keyID)) {
-          return FOOTER_KEY;
-        }
-        if (COLUMN_KEY_ID.equals(keyID)) {
-          return COLUMN_KEY;
-        }
-        return null;
-      }
-    };
-
-    return FileDecryptionProperties.builder().withKeyRetriever(keyRetriever).build();
-  }
-
-  private File createParquetFile(String filename, FileEncryptionProperties encryptionProps) throws IOException {
+  private File createParquetFile(String filename, EncMode mode) throws IOException {
     File file = new File(getTempFolder(), filename);
     Path fsPath = new Path(file.getPath());
-    Configuration conf = new Configuration();
+
+    Configuration conf = getHadoopConfiguration(mode);
+    FileEncryptionProperties fileEncryptionProperties = null;
+
+    try {
+      if (null == conf) {
+        conf = new Configuration();
+      } else {
+        EncryptionPropertiesFactory cryptoFactory = EncryptionPropertiesFactory.loadFactory(conf);
+        fileEncryptionProperties = cryptoFactory.getFileEncryptionProperties(conf, fsPath, null);
+      }
+    } catch (Exception e) {
+      throw new IOException("Failed to create encryption properties for " + filename, e);
+    }
 
     MessageType schema = Types.buildMessage()
-        .required(org.apache.parquet.schema.PrimitiveType.PrimitiveTypeName.INT32)
+        .required(PrimitiveType.PrimitiveTypeName.INT32)
         .named("id")
-        .required(org.apache.parquet.schema.PrimitiveType.PrimitiveTypeName.BINARY)
+        .required(PrimitiveType.PrimitiveTypeName.BINARY)
         .named("ssn")
-        .required(org.apache.parquet.schema.PrimitiveType.PrimitiveTypeName.BINARY)
+        .required(PrimitiveType.PrimitiveTypeName.BINARY)
         .named("name")
         .named("schema");
 
@@ -150,18 +125,15 @@ public class TestEncryptionInfoCommand extends ParquetFileTest {
 
     ExampleParquetWriter.Builder builder = ExampleParquetWriter.builder(fsPath)
         .withConf(conf)
-        .withWriteMode(org.apache.parquet.hadoop.ParquetFileWriter.Mode.OVERWRITE)
+        .withWriteMode(ParquetFileWriter.Mode.OVERWRITE)
         .withCompressionCodec(CompressionCodecName.UNCOMPRESSED)
         .withRowGroupSize(1024)
         .withPageSize(1024)
         .withDictionaryPageSize(512)
         .withDictionaryEncoding(true)
         .withValidation(false)
-        .withWriterVersion(ParquetProperties.WriterVersion.PARQUET_2_0);
-
-    if (encryptionProps != null) {
-      builder.withEncryption(encryptionProps);
-    }
+        .withWriterVersion(ParquetProperties.WriterVersion.PARQUET_2_0)
+        .withEncryption(fileEncryptionProperties);
 
     try (ParquetWriter<Group> writer = builder.build()) {
       for (int i = 0; i < 5; i++) {
@@ -175,26 +147,51 @@ public class TestEncryptionInfoCommand extends ParquetFileTest {
     return file;
   }
 
-  private static void enableCliDecryptionKeys() {
-    System.setProperty(
-        "parquet.crypto.factory.class", "org.apache.parquet.crypto.keytools.PropertiesDrivenCryptoFactory");
-    System.setProperty(
+  private Configuration getHadoopConfiguration(EncMode mode) {
+    if (mode == EncMode.NONE) {
+      return null;
+    }
+
+    Configuration conf = new Configuration();
+
+    conf.set("parquet.crypto.factory.class", FACTORY_CLASS);
+    conf.set("parquet.encryption.kms.client.class", MOCK_KMS_CLASS);
+    conf.set(
         "parquet.encryption.key.list",
-        FOOTER_KEY_ID + ":" + bytesToHex(FOOTER_KEY) + "," + COLUMN_KEY_ID + ":" + bytesToHex(COLUMN_KEY));
+        FOOTER_KEY_ID + ":" + bytesToBase64(FOOTER_KEY) + ", " + COLUMN_KEY_ID + ":"
+            + bytesToBase64(COLUMN_KEY));
+
+    if (mode == EncMode.COLUMN_AND_FOOTER) {
+      conf.set(PropertiesDrivenCryptoFactory.COLUMN_KEYS_PROPERTY_NAME, COLUMN_KEY_ID + ":ssn");
+      conf.set(PropertiesDrivenCryptoFactory.FOOTER_KEY_PROPERTY_NAME, FOOTER_KEY_ID);
+    } else if (mode == EncMode.COLUMN_PLAINTEXT_FOOTER) {
+      conf.set(PropertiesDrivenCryptoFactory.COLUMN_KEYS_PROPERTY_NAME, COLUMN_KEY_ID + ":ssn");
+      conf.set(PropertiesDrivenCryptoFactory.FOOTER_KEY_PROPERTY_NAME, FOOTER_KEY_ID);
+      conf.setBoolean(PropertiesDrivenCryptoFactory.PLAINTEXT_FOOTER_PROPERTY_NAME, true);
+    }
+
+    return conf;
   }
 
-  private static String bytesToHex(byte[] bytes) {
-    StringBuilder sb = new StringBuilder();
-    for (byte b : bytes) {
-      sb.append(String.format("%02x", b));
-    }
-    return sb.toString();
+  private Configuration getDecryptionConfiguration() {
+    Configuration conf = new Configuration();
+    conf.set("parquet.crypto.factory.class", FACTORY_CLASS);
+    conf.set("parquet.encryption.kms.client.class", MOCK_KMS_CLASS);
+    conf.set(
+        "parquet.encryption.key.list",
+        FOOTER_KEY_ID + ":" + bytesToBase64(FOOTER_KEY) + ", " + COLUMN_KEY_ID + ":"
+            + bytesToBase64(COLUMN_KEY));
+    return conf;
+  }
+
+  private static String bytesToBase64(byte[] bytes) {
+    return Base64.getEncoder().encodeToString(bytes);
   }
 
   @Test
   public void testUnencryptedFile() throws IOException {
     EncryptionInfoCommand command = new EncryptionInfoCommand(createLogger());
-    command.targets = java.util.Arrays.asList(unencryptedFile.getAbsolutePath());
+    command.targets = Arrays.asList(unencryptedFile.getAbsolutePath());
     command.setConf(new Configuration());
 
     int result = command.run();
@@ -204,36 +201,33 @@ public class TestEncryptionInfoCommand extends ParquetFileTest {
   @Test
   public void testColumnEncryptedFile() throws IOException {
     EncryptionInfoCommand cmd = new EncryptionInfoCommand(createLogger());
-    cmd.targets = java.util.Arrays.asList(columnEncryptedFile.getAbsolutePath());
+    cmd.targets = Arrays.asList(columnEncryptedFile.getAbsolutePath());
     cmd.setConf(new Configuration());
 
     try {
       cmd.run();
       fail("Expected ParquetCryptoRuntimeException for encrypted file without keys");
-    } catch (org.apache.parquet.crypto.ParquetCryptoRuntimeException e) {
-      assertTrue(e.getMessage().contains("encrypted") || e.getMessage().contains("key"));
+    } catch (ParquetCryptoRuntimeException e) {
+      assertTrue(e.getMessage().toLowerCase().contains("encrypted")
+          || e.getMessage().toLowerCase().contains("key"));
     }
   }
 
   @Test
   public void testFooterEncryptedFile() throws IOException {
     EncryptionInfoCommand cmd = new EncryptionInfoCommand(createLogger());
-    cmd.targets = java.util.Arrays.asList(footerEncryptedFile.getAbsolutePath());
-    cmd.setConf(new Configuration());
+    cmd.targets = Arrays.asList(footerEncryptedFile.getAbsolutePath());
+    cmd.setConf(getDecryptionConfiguration());
 
-    try {
-      cmd.run();
-      fail("Expected ParquetCryptoRuntimeException for encrypted file without keys");
-    } catch (org.apache.parquet.crypto.ParquetCryptoRuntimeException e) {
-      assertTrue(e.getMessage().contains("encrypted") || e.getMessage().contains("key"));
-    }
+    int result = cmd.run();
+    assertEquals(0, result);
   }
 
   @Test
   public void testCommandWorksWithRealFile() throws IOException {
     File file = parquetFile();
     EncryptionInfoCommand command = new EncryptionInfoCommand(createLogger());
-    command.targets = java.util.Arrays.asList(file.getAbsolutePath());
+    command.targets = Arrays.asList(file.getAbsolutePath());
     command.setConf(new Configuration());
 
     int result = command.run();
@@ -242,29 +236,33 @@ public class TestEncryptionInfoCommand extends ParquetFileTest {
 
   @Test
   public void testCommandExists() {
-    EncryptionInfoCommand cmd = new EncryptionInfoCommand(LoggerFactory.getLogger(TestEncryptionInfoCommand.class));
+    new EncryptionInfoCommand(LoggerFactory.getLogger(TestEncryptionInfoCommand.class));
   }
 
   @Test
   public void testColumnEncryptedFileWithKeys() throws IOException {
     assertTrue(columnEncryptedFile.exists());
 
-    enableCliDecryptionKeys();
-
     EncryptionInfoCommand cmd = new EncryptionInfoCommand(createLogger());
-    cmd.targets = java.util.Arrays.asList(columnEncryptedFile.getAbsolutePath());
-    cmd.setConf(new Configuration());
-
+    cmd.targets = Arrays.asList(columnEncryptedFile.getAbsolutePath());
+    cmd.setConf(getDecryptionConfiguration());
     int rc = cmd.run();
     assertEquals(0, rc);
 
-    FileDecryptionProperties decryptionProps = createDecryptionProperties();
-    ParquetReadOptions options =
-        ParquetReadOptions.builder().withDecryption(decryptionProps).build();
-    InputFile inputFile =
-        HadoopInputFile.fromPath(new Path(columnEncryptedFile.getAbsolutePath()), new Configuration());
+    Configuration hadoopConfig = getDecryptionConfiguration();
+    FileDecryptionProperties fileDecryptionProperties = null;
+
+    DecryptionPropertiesFactory cryptoFactory = DecryptionPropertiesFactory.loadFactory(hadoopConfig);
+    fileDecryptionProperties = cryptoFactory.getFileDecryptionProperties(
+        hadoopConfig, new Path(columnEncryptedFile.getAbsolutePath()));
+
+    InputFile inputFile = HadoopInputFile.fromPath(new Path(columnEncryptedFile.getAbsolutePath()), hadoopConfig);
     ParquetMetadata footer;
-    try (ParquetFileReader reader = ParquetFileReader.open(inputFile, options)) {
+    try (ParquetFileReader reader = ParquetFileReader.open(
+        inputFile,
+        ParquetReadOptions.builder()
+            .withDecryption(fileDecryptionProperties)
+            .build())) {
       footer = reader.getFooter();
     }
     Set<String> encrypted = EncryptionInfoCommand.findEncryptedColumns(footer);
@@ -273,30 +271,29 @@ public class TestEncryptionInfoCommand extends ParquetFileTest {
 
   @Test
   public void testFooterEncryptedFileWithKeys() throws IOException {
-    FileDecryptionProperties decryptionProps = createDecryptionProperties();
-    Configuration verifyConf = new Configuration();
+    withLogger((logger, events) -> {
+      EncryptionInfoCommand cmd = new EncryptionInfoCommand(logger);
+      cmd.targets = Arrays.asList(footerEncryptedFile.getAbsolutePath());
+      cmd.setConf(getDecryptionConfiguration());
 
-    ParquetReadOptions readOptions =
-        ParquetReadOptions.builder().withDecryption(decryptionProps).build();
-    InputFile inputFile = HadoopInputFile.fromPath(new Path(footerEncryptedFile.getPath()), verifyConf);
+      int rc = cmd.run();
+      assertEquals(0, rc);
 
-    try {
-      ParquetFileReader.open(inputFile, readOptions);
-      fail("Expected ParquetCryptoRuntimeException for footer encrypted file without keys");
-    } catch (ParquetCryptoRuntimeException e) {
-    }
+      boolean foundColumnSummary = false;
+      boolean foundSsn = false;
 
-    ByteArrayOutputStream out = new ByteArrayOutputStream();
-    enableCliDecryptionKeys();
-    Logger logger = createLogger(new PrintStream(out));
-    EncryptionInfoCommand cmd = new EncryptionInfoCommand(logger);
-    cmd.targets = java.util.Arrays.asList(footerEncryptedFile.getAbsolutePath());
-    cmd.setConf(new Configuration());
+      for (org.slf4j.event.LoggingEvent event : events) {
+        String message = event.getMessage();
+        if (message.contains("Column encryption summary:")) {
+          foundColumnSummary = true;
+        }
+        if (message.contains("ssn")) {
+          foundSsn = true;
+        }
+      }
 
-    int rc = cmd.run();
-    assertEquals(0, rc);
-    String output = out.toString();
-    assertTrue(output.contains("Encryption type: ENCRYPTED_FOOTER"));
-    assertTrue(output.contains("Footer is encrypted"));
+      assertTrue("Should contain column encryption summary", foundColumnSummary);
+      assertTrue("Should contain ssn column", foundSsn);
+    });
   }
 }
