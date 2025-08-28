76e9b252ba46d440f26389b9d2aa442e59742e4c
arnavb <arnavb@uber.com>
2025-08-28 11:31:50 +0000

    address comments

--- parquet-cli/src/main/java/org/apache/parquet/cli/commands/ConvertCommand.java
+++ parquet-cli/src/main/java/org/apache/parquet/cli/commands/ConvertCommand.java
@@ -34,7 +34,6 @@ import java.io.IOException;
 import java.util.List;
 import org.apache.avro.Schema;
 import org.apache.avro.generic.GenericData;
-import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.parquet.avro.AvroParquetWriter;
