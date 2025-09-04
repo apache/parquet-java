8daa6f53649c4a599184a7198214d8f748dbe521
arnavb <arnavb@uber.com>
2025-09-04 16:59:37 +0000

    update

--- parquet-cli/src/main/java/org/apache/parquet/cli/BaseCommand.java
+++ parquet-cli/src/main/java/org/apache/parquet/cli/BaseCommand.java
@@ -66,7 +66,7 @@ public abstract class BaseCommand implements Command, Configurable {
 
   private static final String RESOURCE_URI_SCHEME = "resource";
   private static final String STDIN_AS_SOURCE = "stdin";
-  public static final String PARQUET_CLI_ENABLE_GROUP_READER = "parquet.enable.fallback-simple-reader";
+  public static final String PARQUET_CLI_ENABLE_GROUP_READER = "parquet.enable.simple-reader";
 
   /**
    * Note for dev: Due to legancy reasons, parquet-cli used the avro schema reader which
