96554b71db41c03d8a74b79790c7d5bbbd30a4a7
Andrew Pikler <andrew.pikler@earnix.com>
2025-12-04 12:29:39 +0200

    Add stub methods for SchemaElement conversion refactor scope #1835

---
+++ parquet-column/src/main/java/org/apache/parquet/schema/ParquetSchemaConverter.java
@@ -0,0 +1,66 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.parquet.schema;
+
+import org.apache.parquet.format.ColumnOrder;
+import org.apache.parquet.format.SchemaElement;
+import java.util.List;
+
+/**
+ * This class converts between the thrift based {@link org.apache.parquet.format.SchemaElement} class and the
+ * parquet-column {@link org.apache.parquet.column.ColumnDescriptor} classes.
+ */
+public class ParquetSchemaConverter
+{
+  public ParquetSchemaConverter() {
+  }
+
+  /**
+   * Parse parquet-column MessageType and write the {@link ColumnOrder} objects for it.
+   *
+   * @param schema the {@link MessageType} schema
+   * @return the ordering defined for each of the columns
+   */
+  public List<ColumnOrder> getColumnOrders(MessageType schema) {
+    // todo
+    throw new UnsupportedOperationException();
+  }
+
+  /**
+   * Parse parquet-column MessageType and write the {@link SchemaElement} objects for it.
+   *
+   * @param schema the {@link MessageType} schema
+   * @return the {@link SchemaElement} objects for this parquet-column schema
+   */
+  public List<SchemaElement> toParquetSchema(MessageType schema) {
+    // todo.
+    throw new UnsupportedOperationException();
+  }
+
+  /**
+   * Create parquet-column MessageType from {@link SchemaElement} and {@link ColumnOrder} thrift objects
+   *
+   * @param schema the {@link MessageType} schema
+   * @return the ordering defined for each of the columns
+   */
+  public MessageType fromParquetSchema(List<SchemaElement> schema, List<ColumnOrder> columnOrders) {
+    // todo
+    throw new UnsupportedOperationException();
+  }
+}
