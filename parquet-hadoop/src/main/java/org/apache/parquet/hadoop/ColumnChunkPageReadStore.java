a79dc3a4dc5ddbf61beebd0cb63ccf6ba2f1b87e
1fanwang <1fannnw@gmail.com>
2026-09-18 03:14:51 -0700

    GH-3150: Keep column values intact with direct codecs
    Signed-off-by: 1fanwang <1fannnw@gmail.com>

--- parquet-hadoop/src/main/java/org/apache/parquet/hadoop/ColumnChunkPageReadStore.java
+++ parquet-hadoop/src/main/java/org/apache/parquet/hadoop/ColumnChunkPageReadStore.java
@@ -177,7 +177,9 @@ class ColumnChunkPageReadStore implements PageReadStore, DictionaryPageReadStore
                 bytes = BytesInput.from(blockDecryptor.decrypt(bytes.toByteArray(), dataPageAAD));
               }
               long start = System.nanoTime();
-              decompressed = decompressor.decompress(bytes, dataPageV1.getUncompressedSize());
+              decompressed = decompressor
+                  .decompress(bytes, dataPageV1.getUncompressedSize())
+                  .copy(releaser);
               setDecompressMetrics(bytes, start);
             }
 
@@ -257,7 +259,9 @@ class ColumnChunkPageReadStore implements PageReadStore, DictionaryPageReadStore
                     - dataPageV2.getDefinitionLevels().size()
                     - dataPageV2.getRepetitionLevels().size());
                 long start = System.nanoTime();
-                pageBytes = decompressor.decompress(pageBytes, uncompressedSize);
+                pageBytes = decompressor
+                    .decompress(pageBytes, uncompressedSize)
+                    .copy(releaser);
                 setDecompressMetrics(pageBytes, start);
               }
             }
@@ -326,7 +330,9 @@ class ColumnChunkPageReadStore implements PageReadStore, DictionaryPageReadStore
         long start = System.nanoTime();
         setDecompressMetrics(bytes, start);
         DictionaryPage decompressedPage = new DictionaryPage(
-            decompressor.decompress(bytes, compressedDictionaryPage.getUncompressedSize()),
+            decompressor
+                .decompress(bytes, compressedDictionaryPage.getUncompressedSize())
+                .copy(releaser),
             compressedDictionaryPage.getDictionarySize(),
             compressedDictionaryPage.getEncoding());
         if (compressedDictionaryPage.getCrc().isPresent()) {
